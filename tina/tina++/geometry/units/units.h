2e876cffe6cf12d4eb4b4dea141454d627dfb77a
Richard Liebscher <richard_liebscher@turag.de>
2013-11-01 17:39:45 +0100

    [tina] Einheiten über User-Literals ab GCC 4.7

--- tina/tina++/geometry/units/units.h
+++ tina/tina++/geometry/units/units.h
@@ -19,6 +19,24 @@ constexpr Unit<AngleDimension>  deg(FROM_SI_CONVERT_FACTOR_ANGLE * M_PI/180.f);
 constexpr Unit<TimeDimension>  s(FROM_SI_CONVERT_FACTOR_TIME);
 constexpr Unit<TimeDimension> ms(FROM_SI_CONVERT_FACTOR_TIME / 1000.f);
 
+#if GCC_VERSION > 40700 && defined(TINA_UNITS_LITERALS)
+constexpr Length operator"" _mm(long double x) { return x * mm; }
+constexpr Length operator"" _mm(long long unsigned x) { return x * mm; }
+constexpr Length operator"" _cm(long double x) { return x * cm; }
+constexpr Length operator"" _cm(long long unsigned x) { return x * cm; }
+constexpr Length operator"" _dm(long double x) { return x * dm; }
+constexpr Length operator"" _dm(long long unsigned x) { return x * dm; }
+constexpr Length operator"" _m(long double x) { return x * m; }
+constexpr Length operator"" _m(long long unsigned x) { return x * m; }
+
+constexpr Angle operator"" _mrad(long double x) { return x * mrad; }
+constexpr Angle operator"" _mrad(long long unsigned x) { return x * mrad; }
+constexpr Angle operator"" _rad(long double x) { return x * rad; }
+constexpr Angle operator"" _rad(long long unsigned x) { return x * rad; }
+constexpr Angle operator"" _deg(long double x) { return x * deg; }
+constexpr Angle operator"" _deg(long long unsigned x) { return x * deg; }
+#endif
+
 } // namespace Units
 } // namespace TURAG
 
