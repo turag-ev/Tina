8d310b7ab58aa24134c53d3bb2bd592dfe62bab2
Martin Oemus <martin@oemus.net>
2013-11-12 02:13:09 +0100

    [feldbus] basis-slave-Implementierung sollte fertig sein

--- tina/tina/feldbus/slave/feldbus.c
+++ tina/tina/feldbus/slave/feldbus.c
@@ -10,40 +10,42 @@
 #include <tina/crc/xor.h>
 #include <tina/crc/crc8.h>
 
-#ifdef TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
-#warning TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
-#endif
 #ifndef MY_ADDR
-#error MY_ADDR must be defined
+# error MY_ADDR must be defined
 #endif
 #ifndef TURAG_FELDBUS_SLAVE_CONFIG_CRC_TYPE
-#error TURAG_FELDBUS_SLAVE_CONFIG_CRC_TYPE must be defined
-#endif
-#ifndef TURAG_FELDBUS_SLAVE_CONFIG_BOOTLOADER_AVAILABLE
-#error TURAG_FELDBUS_SLAVE_CONFIG_BOOTLOADER_AVAILABLE must be defined
+# error TURAG_FELDBUS_SLAVE_CONFIG_CRC_TYPE must be defined
 #endif
 #ifndef TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE
-#error TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE must be defined
+# error TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE must be defined
+#endif
+#ifndef TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+# error TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED must be defined
+#else
+# if TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+#  warning TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+#  if TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE < 13
+#   error TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE < 13 and needs to be bigger.
+#  endif
+# endif
 #endif
 
 
+static void start_transmission();
 
 
-
-typedef struct {
+static struct {
 	uint8_t rxbuf[TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE];
 	uint8_t txbuf[TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE];
 	uint8_t length;
 	uint8_t index;
 	uint8_t overflow;
 	uint8_t chksum;
-#ifdef TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+#if TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
 	volatile uint8_t transmission_active;
 	uint8_t chksum_required;
 #endif
-} turag_feldbus_slave_uart_t;
-
-static turag_feldbus_slave_uart_t uart;
+} uart;
 
 
 
@@ -51,11 +53,15 @@ void turag_feldbus_slave_init() {
 	uart.txbuf[0] = TURAG_FELDBUS_MASTER_ADDR|MY_ADDR;
 	uart.index = 0;
 	uart.overflow = 0;
-
-#ifdef TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+#if TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
 	uint8_t transmission_active = 0;
 #endif
+
+	turag_feldbus_slave_rts_off();
+	turag_feldbus_slave_activate_rx_interrupt();
+
 }
+
 
 void turag_feldbus_slave_byte_received(uint8_t data) {
 	uart.rxbuf[uart.index] = data;
@@ -70,23 +76,6 @@ void turag_feldbus_slave_byte_received(uint8_t data) {
 	turag_feldbus_slave_start_receive_timeout();
 }
 
-void turag_feldbus_slave_ready_to_transmit() {
-	if (uart.index == uart.length) {
-		// all data bytes are processed, now transmit checksum
-		turag_feldbus_slave_transmit_byte(uart.chksum);
-	} else {
-		turag_feldbus_slave_transmit_byte(uart.txbuf[uart.index]);
-		++uart.index;
-	}
-}
-
-void turag_feldbus_slave_transmission_complete() {
-	// turn off transmitter and release bus, turn on receiver
-	turag_feldbus_slave_prepare_receiving();
-
-	uart.index = 0;
-	uart.transmission_active = 0;
-}
 
 void turag_feldbus_slave_receive_timeout_occured() {
 	if (uart.overflow) goto CLEANUP;
@@ -101,14 +90,24 @@ void turag_feldbus_slave_receive_timeout_occured() {
 
 	// message addressed to me?
 	if (uart.rxbuf[0] == MY_ADDR) {
-		uart.length = turag_feldbus_slave_process_package(uart.rxbuf + 1, uart.index - 2, uart.txbuf + 1) + 1;
+		if (uart.index == 2) {
+			// we received a ping request -> respond with empty packet (address + checksum)
+			uart.length = 1;
+		} else if (uart.rxbuf[1] == 0) {
+			// received a debug packet
+			// TODO
+		} else {
+			// received some other packet --> let somebody else process it
+			uart.length = turag_feldbus_slave_process_package(uart.rxbuf + 1, uart.index - 2, uart.txbuf + 1) + 1;
+		}
+
+		// calculate correct checksum and initiate transmission
 #if TURAG_FELDBUS_SLAVE_CONFIG_CRC_TYPE == TURAG_FELDBUS_CHECKSUM_XOR
-		uart.txbuf[uart.length] = xor_checksum_calculate(uart.txbuf, uart.length);
+		uart.chksum = xor_checksum_calculate(uart.txbuf, uart.length);
 #elif TURAG_FELDBUS_SLAVE_CONFIG_CRC_TYPE == TURAG_FELDBUS_CHECKSUM_CRC8_ICODE
-		uart.txbuf[uart.length] = turag_crc8_calculate(uart.txbuf, uart.length);
+		uart.chksum = turag_crc8_calculate(uart.txbuf, uart.length);
 #endif
-		++uart.length;
-#ifdef TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+#if TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
 		uart.chksum_required = 1;
 #endif
 		start_transmission();
@@ -118,7 +117,229 @@ void turag_feldbus_slave_receive_timeout_occured() {
 
 
 CLEANUP:
-	uart.chksum = 0;
 	uart.index = 0;
 	uart.overflow = 0;
+}
+
+
+static void start_transmission() {
+#if TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+		uart.transmission_active = 1;
+#endif
+	turag_feldbus_slave_deactivate_rx_interrupt();
+	turag_feldbus_slave_rts_on();
+	turag_feldbus_slave_activate_dre_interrupt();
+}
+
+
+
+void turag_feldbus_slave_ready_to_transmit() {
+	if (uart.index == uart.length) {
+#if TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+		if (uart.chksum_required)
+#endif
+			// all data bytes are processed, now transmit checksum
+			turag_feldbus_slave_transmit_byte(uart.chksum);
+
+		turag_feldbus_slave_deactivate_dre_interrupt();
+		turag_feldbus_slave_activate_tx_interrupt();
+	} else {
+		turag_feldbus_slave_transmit_byte(uart.txbuf[uart.index]);
+		++uart.index;
+	}
+}
+
+void turag_feldbus_slave_transmission_complete() {
+	// turn off transmitter and release bus, turn on receiver
+	turag_feldbus_slave_rts_off();
+	turag_feldbus_slave_deactivate_tx_interrupt();
+	turag_feldbus_slave_activate_rx_interrupt();
+
+	uart.index = 0;
+#if TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+	uart.transmission_active = 0;
+#endif
+}
+
+
+
+static uint8_t digit_to_hex(uint8_t dig) {
+	dig &= 0xF;
+	dig += '0';
+	if (dig > '9') dig += 'A' - '0' - 10;
+	return dig;
+}
+
+
+// public debug functions
+void print_text(const char *buf) {
+	while (uart.transmission_active);
+
+	uint8_t i = 0;
+
+	while(*buf) {
+		uart.txbuf[i] = *buf;
+		++i;
+		++buf;
+
+		if (i >= TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE) {
+			i = 1;
+			break;
+		}
+	}
+	uart.length = i;
+	uart.chksum_required = 0;
+	start_transmission();
+}
+
+void print_char(uint8_t x) {
+	while (uart.transmission_active);
+
+	uart.txbuf[0] = '0';
+	uart.txbuf[1] = 'x';
+	uart.txbuf[2] = digit_to_hex(x >> 4);
+	uart.txbuf[3] = digit_to_hex(x);
+
+	uart.length = 4;
+	uart.chksum_required = 0;
+	start_transmission();
+}
+
+void print_short(uint16_t x) {
+	while (uart.transmission_active);
+
+	uart.txbuf[0] = '0';
+	uart.txbuf[1] = 'x';
+	uart.txbuf[2] = digit_to_hex(x >> 12);
+	uart.txbuf[3] = digit_to_hex(x >> 8);
+	uart.txbuf[4] = digit_to_hex(x >> 4);
+	uart.txbuf[5] = digit_to_hex(x);
+	uart.txbuf[6] = '\n';
+	uart.txbuf[7] = '\r';
+
+	uart.length = 8;
+	uart.chksum_required = 0;
+	start_transmission();
+}
+void print_short_nn(uint16_t x) {
+	while (uart.transmission_active);
+
+	uart.txbuf[0] = '0';
+	uart.txbuf[1] = 'x';
+	uart.txbuf[2] = digit_to_hex(x >> 12);
+	uart.txbuf[3] = digit_to_hex(x >> 8);
+	uart.txbuf[4] = digit_to_hex(x >> 4);
+	uart.txbuf[5] = digit_to_hex(x);
+
+	uart.length = 6;
+	uart.chksum_required = 0;
+	uart.transmission_active = 1;
+	start_transmission();
+}
+
+void print_sshort(int16_t x) {
+	while (uart.transmission_active);
+
+	if (x<0) {
+		uart.txbuf[0] = '-';
+		x=-x;
+	} else uart.txbuf[0] = '+';
+
+	uart.txbuf[1] = '0';
+	uart.txbuf[2] = 'x';
+	uart.txbuf[3] = digit_to_hex(x >> 12);
+	uart.txbuf[4] = digit_to_hex(x >> 8);
+	uart.txbuf[5] = digit_to_hex(x >> 4);
+	uart.txbuf[6] = digit_to_hex(x);
+	uart.txbuf[7] = '\n';
+	uart.txbuf[8] = '\r';
+
+	uart.length = 9;
+	uart.chksum_required = 0;
+	start_transmission();
+}
+void print_sshort_nn(int16_t x) {
+	while (uart.transmission_active);
+
+	if (x<0) {
+		uart.txbuf[0] = '-';
+		x=-x;
+	} else uart.txbuf[0] = '+';
+
+	uart.txbuf[1] = '0';
+	uart.txbuf[2] = 'x';
+	uart.txbuf[3] = digit_to_hex(x >> 12);
+	uart.txbuf[4] = digit_to_hex(x >> 8);
+	uart.txbuf[5] = digit_to_hex(x >> 4);
+	uart.txbuf[6] = digit_to_hex(x);
+
+	uart.length = 7;
+	uart.chksum_required = 0;
+	start_transmission();
+}
+
+void print_long(uint32_t x) {
+	while (uart.transmission_active);
+
+	uart.txbuf[0] = '0';
+	uart.txbuf[1] = 'x';
+	uart.txbuf[2] = digit_to_hex(x >> 28);
+	uart.txbuf[3] = digit_to_hex(x >> 24);
+	uart.txbuf[4] = digit_to_hex(x >> 20);
+	uart.txbuf[5] = digit_to_hex(x >> 16);
+	uart.txbuf[6] = digit_to_hex(x >> 12);
+	uart.txbuf[7] = digit_to_hex(x >> 8);
+	uart.txbuf[8] = digit_to_hex(x >> 4);
+	uart.txbuf[9] = digit_to_hex(x);
+	uart.txbuf[10] = '\n';
+	uart.txbuf[11] = '\r';
+
+
+	uart.length = 12;
+	uart.chksum_required = 0;
+	start_transmission();
+}
+
+void print_short_d(int16_t x) {
+	while (uart.transmission_active);
+
+	uart.txbuf[0] = ' ';
+	uart.txbuf[1] = ' ';
+	uart.txbuf[2] = ' ';
+	uart.txbuf[3] = ' ';
+	uart.txbuf[4] = ' ';
+	uart.txbuf[5] = ' ';
+	uart.txbuf[6] = '\n';
+	uart.txbuf[7] = '\r';
+	itoa(x, (char*)uart.txbuf, 10);
+
+	uart.length = 8;
+	uart.chksum_required = 0;
+	start_transmission();
+}
+
+void print_slong(int32_t x) {
+	while (uart.transmission_active);
+
+	if (x<0) {
+		uart.txbuf[0] = '-';
+		x=-x;
+	} else uart.txbuf[0] = '+';
+
+	uart.txbuf[1] = '0';
+	uart.txbuf[2] = 'x';
+	uart.txbuf[3] = digit_to_hex(x >> 28);
+	uart.txbuf[4] = digit_to_hex(x >> 24);
+	uart.txbuf[5] = digit_to_hex(x >> 20);
+	uart.txbuf[6] = digit_to_hex(x >> 16);
+	uart.txbuf[7] = digit_to_hex(x >> 12);
+	uart.txbuf[8] = digit_to_hex(x >> 8);
+	uart.txbuf[9] = digit_to_hex(x >> 4);
+	uart.txbuf[10] = digit_to_hex(x);
+	uart.txbuf[11] = '\n';
+	uart.txbuf[12] = '\r';
+
+	uart.length = 13;
+	uart.chksum_required = 0;
+	start_transmission();
 }
--- tina/tina/feldbus/slave/feldbus.h
+++ tina/tina/feldbus/slave/feldbus.h
@@ -16,10 +16,31 @@
 // hardware interfaceing functions that are used by this module
 // and need to be defined for the target platform
 // ---------------------------------------------------
-extern void turag_feldbus_slave_prepare_receiving();
-extern void turag_feldbus_slave_prepare_transmitting();
+
+extern void turag_feldbus_slave_rts_off();
+extern void turag_feldbus_slave_rts_on();
+
+extern void turag_feldbus_slave_activate_dre_interrupt();
+extern void turag_feldbus_slave_deactivate_dre_interrupt();
+extern void turag_feldbus_slave_activate_rx_interrupt();
+extern void turag_feldbus_slave_deactivate_rx_interrupt();
+extern void turag_feldbus_slave_activate_tx_interrupt();
+extern void turag_feldbus_slave_deactivate_tx_interrupt();
+
+/**
+ * Starts some component that is able to call turag_feldbus_slave_receive_timeout_occured.
+ * The timeout is specified in the protocol definition. Whenever the function is called
+ * it is required that the timer starts counting from the start regardless of its previous state.
+ */
 extern void turag_feldbus_slave_start_receive_timeout();
+
+/**
+ * Transmits on byte over the bus.
+ * @param byte Data to send
+ */
 extern void turag_feldbus_slave_transmit_byte(uint8_t byte);
+
+
 
 
 // ---------------------------------------------------
@@ -27,7 +48,23 @@ extern void turag_feldbus_slave_transmit_byte(uint8_t byte);
 // and should be defined by the device protocol
 // implementation
 // ---------------------------------------------------
+/**
+ * This function gets called when the device received a package.
+ * The response should be made as soon as possible.
+ *
+ * @param message			Buffer holding the received data
+ * @param message_length	Size of received data
+ * @param response			Buffer that can be filled with the response data.
+ * @return					Number of bytes put in the response buffer. You must not return more than TURAG_FELDBUS_SLAVE_CONFIG_BUFFER_SIZE - 1 bytes.
+ */
 extern uint8_t turag_feldbus_slave_process_package(uint8_t* message, uint8_t message_length, uint8_t* response);
+
+/**
+ * This function gets called when the device received a broadcast package.
+ *
+ * @param message			Buffer holding the received data
+ * @param message_length	Size of received data
+ */
 extern void turag_feldbus_slave_process_broadcast(uint8_t* message, uint8_t message_length);
 
 
@@ -38,16 +75,69 @@ extern void turag_feldbus_slave_process_broadcast(uint8_t* message, uint8_t mess
 // and the device protocol implementation
 // is supposed to make use of
 // ---------------------------------------------------
+/**
+ * Initialize TURAG Feldbus support
+ * Call this function after initializing the UART peripheral
+ * with the correct baudrate and after enabling transmitter and receiver.
+ *
+ * If your device does not have a bootloader, you should call this function
+ * only after making sure that no device is being programmed.
+ */
 void turag_feldbus_slave_init();
 
 // ---------------------------------------------------
 // functions provided by this module
 // which should be called by the hardware driver
 // ---------------------------------------------------
+/**
+ * Receive-Complete-Interrupt Interface.
+ * Call this function from the Receive-Complete-Interrupt on your system.
+ * @param byte Received byte
+ */
 void turag_feldbus_slave_byte_received(uint8_t byte);
+
+/**
+ * Data-Register-Empty-Interrupt-Interface.
+ * Call this function from the Data-Register-Empty-Interrupt on your system.
+ */
 void turag_feldbus_slave_ready_to_transmit();
+
+/**
+ * Transmit-Complete-Interrupt-Interface.
+ * Call this function from the Transmit-Complete-Interrupt on your system.
+ */
 void turag_feldbus_slave_transmission_complete();
+
+/**
+ * Timeout occured.
+ * Call this fucntion when the receive timeout has occured.
+ */
 void turag_feldbus_slave_receive_timeout_occured();
+
+
+
+// debugging functions
+#ifdef TURAG_FELDBUS_SLAVE_CONFIG_DEBUG_ENABLED
+	void print_text(const char*);	// print some text
+	void print_char(uint8_t);			// print one byte in hexadecimal format
+	void print_short(uint16_t);		// print two-byte value in hexadecimal format
+	void print_sshort(int16_t x);
+	void print_long(uint32_t x);
+	void print_short_d(int16_t x);
+	void print_slong(int32_t);
+	void print_sshort_nn(int16_t x);
+	void print_short_nn(uint16_t x);
+#else
+# 	define print_text(x)
+# 	define print_char(x)
+# 	define print_short(x)
+# 	define print_sshort(x)
+# 	define print_long(x)
+# 	define print_short_d(x)
+# 	define print_slong(x)
+# 	define print_sshort_nn(x)
+# 	define print_short_nn(x)
+#endif
 
 
 #endif /* TINA_FELDBUS_SLAVE_FELDBUS_H_ */
