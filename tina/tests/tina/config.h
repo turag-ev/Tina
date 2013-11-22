9ce09977dbf477e58baf76f720835ca9fa6c83fd
Richard Liebscher <richard_liebscher@turag.de>
2013-11-22 16:18:00 +0100

    [sc] Tests für SC::Cost. SC::Cost verbessert.

---
+++ tina/tests/tina/config.h
@@ -0,0 +1,14 @@
+#ifndef TINA_SIM_CONFIG_H
+#define TINA_SIM_CONFIG_H
+
+#include <tina/tina.h>
+
+// Attribute für Thread-Startfunktion
+#define TURAG_THREAD_ENTRY 
+
+// Wenn definiert:
+// Nach Aufruf von EventQueue::main() nicht mehr zurückkehren
+// ! dadurch wird EventQueue::event_quit ignoriert
+//#undef TURAG_STATEMACHINE_FOREVER
+
+#endif // TINA_SIM_CONFIG_H
