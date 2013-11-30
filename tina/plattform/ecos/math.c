0d95b8357f03e60d307751cbf778041c71280c39
Richard Liebscher <richard_liebscher@turag.de>
2013-11-30 19:55:03 +0100

    [mc] nur noch Verwendung von float (implizite Umwandlung in double wird vermieden). außerdem viele Warnungen hinzugefügt. Verwendung der neuen inline Funktionen aus Tina.

--- tina/plattform/ecos/math.c
+++ tina/plattform/ecos/math.c
@@ -1,28 +1,8 @@
 
-#include <math.h>
+#include <tina/float.h>
 
 // fuck eCos, we make our own float math functions from eCos double functions
 
-float sinf(float x) {
-  return sin(x);
-}
-
-float asinf(float x) {
-  return asin(x);
-}
-
-float cosf(float x) {
-  return cos(x);
-}
-
-float atan2f(float y, float x) {
-  return atan2(y, x);
-}
-
-float sqrtf(float x) {
-  return sqrt(x);
-}
-
 long int lroundf(float x) {
-  return (x < 0.0f) ? ceil(x - 0.5f) : floor(x + 0.5f);
+  return (x < 0.0f) ? ceilf(x - 0.5f) : floorf(x + 0.5f);
 }
