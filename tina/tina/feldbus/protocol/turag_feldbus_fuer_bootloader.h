185c659672d393347e7631c07f794a4fbe54e28e
Florian Voelker <voelker_florian@turag.de>
2014-02-05 22:42:38 +0100

    [Atmega-Bootloader] Fehlendes Protokoll hinzugefügt

---
+++ tina/tina/feldbus/protocol/turag_feldbus_fuer_bootloader.h
@@ -0,0 +1,14 @@
+#ifndef TURAG_FELDBUS_FUER_BOOTLOADER_H_
+#define TURAG_FELDBUS_FUER_BOOTLOADER_H_
+
+/* 
+ * This header file contains the specification for the protocol
+ * "TURAG-FeldBus für Bootloader". Refer to the DokuWiki for documentation.
+ */
+
+#include "turag_feldbus_bus_protokoll.h"
+
+// device types
+#define TURAG_FELDBUS_BOOTLOADER_GENERIC		0x01
+
+#endif
