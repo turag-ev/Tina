6b8da7da81d5a0cefaaa55e3492c7344e365df75
Florian Voelker <voelker_florian@turag.de>
2014-02-25 01:11:25 +0100

    [AVR-Bootloader] Broadcast kann Bootloader starten; Pagewrite auf zwei Bytes verlängert

--- tina/tina/feldbus/protocol/turag_feldbus_fuer_bootloader.h
+++ tina/tina/feldbus/protocol/turag_feldbus_fuer_bootloader.h
@@ -1,6 +1,6 @@
 /**
  *  @brief		TURAG Feldbus für Bootloader protocol definitions.
- *  @file		turag_feldbus_fuer_aseb.h
+ *  @file		turag_feldbus_fuer_bootloader.h
  *  @date		21.02.2014
  *  @author		Florian Völker <flo-voelker@gmx.de>
  *  @ingroup	feldbus-protocol
@@ -15,15 +15,16 @@
 #include "turag_feldbus_bus_protokoll.h"
 
 // device types
-#define TURAG_FELDBUS_BOOTLOADER_GENERIC		0x01
+#define TURAG_FELDBUS_BOOTLOADER_GENERIC						0x01
 
 // command PC => Bootloader
-#define TURAG_FELDBUS_BOOTLOADER_COMMAND_START_PROGRAMM		0xFF
-#define TURAG_FELDBUS_BOOTLOADER_COMMAND_PAGE_WRITE			0xAA
+#define TURAG_FELDBUS_BOOTLOADER_COMMAND_START_PROGRAMM			0xFF
+#define TURAG_FELDBUS_BOOTLOADER_COMMAND_PAGE_WRITE				0xAA
+#define TURAG_FELDBUS_BOOTLOADER_COMMAND_ENTER_BOOTLOADER		0x00	//As Broadcast
 
 
 // command Bootloader => PC
-#define TURAG_FELDBUS_BOOTLOADER_COMMAND_B_PAGE_FAIL_SIZE	0xFF
+#define TURAG_FELDBUS_BOOTLOADER_COMMAND_B_PAGE_FAIL_SIZE		0xFF
 #define TURAG_FELDBUS_BOOTLOADER_COMMAND_B_PAGE_CORRECT_SIZE	0x11
 
 #endif
