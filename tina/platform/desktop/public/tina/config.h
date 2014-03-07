8a31e16c39d58a355e9dda25f1af4cc71f6944d5
Martin Oemus <martin@oemus.net>
2014-03-07 15:18:11 +0100

    [AVR-Bootloader] launcher Makefile überarbeitet

--- tina/platform/desktop/public/tina/config.h
+++ tina/platform/desktop/public/tina/config.h
@@ -3,13 +3,6 @@
 
 #include <tina/tina.h>
 
-/// Attribute für Thread-Startfunktion
-#define TURAG_THREAD_ENTRY 
-
-/// Wenn definiert:
-/// Nach Aufruf von EventQueue::main() nicht mehr zurückkehren
-/// ! dadurch wird EventQueue::event_quit ignoriert
-//#undef TURAG_THREADS_RUN_FOREVER
 
 /// ASCII-Zeichen für neue Zeile für Debugausgaben
 #define TURAG_DEBUG_NEWLINE "\n"
