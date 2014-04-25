630ae1c38d15ef6e70b6ff0e176e6ccf40d03891
Martin Oemus <martin@oemus.net>
2014-04-26 00:25:29 +0200

    [tina, Console] Neue Option zur Anzeige von Graphen

--- tina/tina/debug/graph.h
+++ tina/tina/debug/graph.h
@@ -29,12 +29,27 @@ unsigned turag_graph_create(const char* name);
 /// \brief Add channels to a diagram and name them.
 /// \param index Index of the diagram to add the channels to.
 /// \param title Title of the channel.
-void turag_graph_channel_title(unsigned index, const char* title);
+void turag_graph_add_channel(unsigned index, const char* title);
 
 #else // DOXYGEN
 
-# define turag_graph_channel_title(index, title) \
-    turag_debug_printf(TURAG_DEBUG_LINE_PREFIX TURAG_DEBUG_GRAPH_TITLE TURAG_DEBUG_GRAPH_PREFIX "%d %s" TURAG_DEBUG_NEWLINE, index, title)
+# define turag_graph_add_channel(index, title) \
+    turag_debug_printf(TURAG_DEBUG_LINE_PREFIX TURAG_DEBUG_GRAPH_TITLE TURAG_DEBUG_GRAPH_PREFIX "%d 0 %s" TURAG_DEBUG_NEWLINE, index, title)
+
+#endif // DOXYGEN
+
+#ifdef DOXYGEN
+
+/// \brief Add channels to a diagram and name them.
+/// \param index Index of the diagram to add the channels to.
+/// \param title Title of the channel.
+/// \param time Length of the visible part of the x-axis.
+void turag_graph_add_channel_fixed_time(unsigned index, const char* title, unsigned time);
+
+#else // DOXYGEN
+
+# define turag_graph_add_channel_fixed_time(index, title, time) \
+    turag_debug_printf(TURAG_DEBUG_LINE_PREFIX TURAG_DEBUG_GRAPH_TITLE TURAG_DEBUG_GRAPH_PREFIX "%d %d %s" TURAG_DEBUG_NEWLINE, index, time, title)
 
 #endif // DOXYGEN
 
