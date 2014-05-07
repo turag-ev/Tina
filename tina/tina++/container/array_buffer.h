847be9e81146e6cee77522017748e615f5f7ce0f
Stefan Ecklebe <stefan.ecklebe@mailbox.tu-dresden.de>
2014-05-07 14:59:03 +0200

    work on collision interface and gneral action structure

--- tina/tina++/container/array_buffer.h
+++ tina/tina++/container/array_buffer.h
@@ -90,13 +90,13 @@ public:
   // ctor
   /// ein ArrayBuffer erstellen
   constexpr explicit ArrayBuffer() :
-    length_(0), bytes_()
+	length_(0), bytes_()
   { }
 
   /// \brief ein ArrayBuffer zerstören
   /// \warning nie direkt aufrufen, für das Entfernen aller Elemente \a ArrayBuffer::clear benutzen.
   ~ArrayBuffer() {
-  	bytes_.erase(begin(), end());
+	bytes_.erase(begin(), end());
   }
 
 #ifndef DOXYGEN
@@ -108,120 +108,120 @@ public:
   // iterators
   /// gibt Zeiger auf erstes Element zurück
   iterator begin() {
-    return iterator(&bytes_[0]);
+	return iterator(&bytes_[0]);
   }
   /// gibt Zeiger auf erstes Element zurück
   constexpr
   const_iterator begin() const {
-    return const_iterator(&bytes_[0]);
+	return const_iterator(&bytes_[0]);
   }
   /// gibt Zeiger auf erstes Element zurück
   ///
   /// im Unterschied zu \a ArrayBuffer::begin wird immer ein \a ArrayBuffer::const_iterator zurückgegeben
   constexpr
   const_iterator cbegin() const {
-    return const_iterator(&bytes_[0]);
+	return const_iterator(&bytes_[0]);
   }
 
   /// gibt Zeiger auf letzes Element zurück, fürs Iterieren in umgekehrter Reihenfolge
   reverse_iterator rbegin() {
-    return reverse_iterator(end());
+	return reverse_iterator(end());
   }
   /// gibt Zeiger auf letzes Element zurück, fürs Iterieren in umgekehrter Reihenfolge
   constexpr
   const_reverse_iterator rbegin() const {
-    return reverse_iterator(end());
+	return reverse_iterator(end());
   }
   /// gibt Zeiger auf letzes Element zurück, fürs Iterieren in umgekehrter Reihenfolge
   ///
   /// im Unterschied zu \a ArrayBuffer::rbegin wird immer ein \a ArrayBuffer::const_reverse_iterator zurückgegeben
   constexpr
   const_reverse_iterator crbegin() const {
-    return reverse_iterator(end());
+	return reverse_iterator(end());
   }
 
   /// gibt Zeiger auf letzes Element zurück
   iterator end() {
-    return iterator(&bytes_[size()]);
+	return iterator(&bytes_[size()]);
   }
   /// gibt Zeiger auf letzes Element zurück
   constexpr
   const_iterator end() const {
-    return const_iterator(&bytes_[size()]);
+	return const_iterator(&bytes_[size()]);
   }
   /// gibt Zeiger auf letzes Element zurück
   ///
   /// im Unterschied zu \a ArrayBuffer::end wird immer ein \a ArrayBuffer::const_iterator zurückgegeben
   constexpr
   const_iterator cend() const {
-    return const_iterator(&bytes_[size()]);
+	return const_iterator(&bytes_[size()]);
   }
 
   /// gibt Zeiger auf erstes Element zurück, fürs Iterieren in umgekehrter Reihenfolge
   reverse_iterator rend() {
-    return reverse_iterator(begin());
+	return reverse_iterator(begin());
   }
   /// gibt Zeiger auf erstes Element zurück, fürs Iterieren in umgekehrter Reihenfolge
   constexpr
   const_reverse_iterator rend() const {
-    return const_reverse_iterator(begin());
+	return const_reverse_iterator(begin());
   }
   /// gibt Zeiger auf erstes Element zurück, fürs Iterieren in umgekehrter Reihenfolge
   ///
   /// im Unterschied zu \a ArrayBuffer::rend wird immer ein \a ArrayBuffer::const_reverse_iterator zurückgegeben
   constexpr
   const_reverse_iterator crend() const {
-    return const_reverse_iterator(begin());
+	return const_reverse_iterator(begin());
   }
 
   // capacity
   /// Gibt zurück, ob sich keine Elemente in Array befinden
   constexpr
   bool empty() const {
-    return length_ == 0;
+	return length_ == 0;
   }
 
   /// Gibt Anzahl der im Moment im Array befindlichen Elemente zurück
   constexpr
   size_type size() const {
-    return length_;
+	return length_;
   }
   /// Gibt Anzahl der im Moment im Array befindlichen Elemente zurück
   constexpr
   size_type length() const {
-    return length_;
+	return length_;
   }
 
   /// Gibt \a N zurück
   constexpr
   size_type capacity() const {
-    return N;
+	return N;
   }
   /// Gibt \a N zurück
   constexpr
   size_type max_size() const {
-    return capacity();
+	return capacity();
   }
 
   // element access
   /// Gibt Referenz auf erstes Element zurück
   reference front() {
-    return *begin();
+	return *begin();
   }
   /// Gibt Referenz auf erstes Element zurück
   constexpr
   const_reference front() const {
-    return *begin();
+	return *begin();
   }
 
   /// Gibt Referenz auf letztes Element zurück
   constexpr
   const_reference back() const {
-    return *(end()-1);
+	return *(end()-1);
   }
   /// Gibt Referenz auf letztes Element zurück
   reference back() {
-    return *(end()-1);
+	return *(end()-1);
   }
 
   ///@{
@@ -230,7 +230,7 @@ public:
   /// \pre n < ArrayBuffer::length()
   /// \param n Index von Element
   reference operator[](size_type n) {
-    return bytes_[n];
+	return bytes_[n];
   }
 
   /// Zugriff als n-tes Element
@@ -239,7 +239,7 @@ public:
   /// \param n Index von Element
   constexpr
   const_reference operator[](size_type n) const {
-    return bytes_[n];
+	return bytes_[n];
   }
   ///@}
 
@@ -258,7 +258,7 @@ public:
   /// // array = { 2 }
   /// \endcode
   void pop_front() {
-    erase(begin());
+	erase(begin());
   }
 
   /// letztes Element entfernen
@@ -274,10 +274,10 @@ public:
   /// // array = { 1 }
   /// \endcode
   void pop_back() {
-    if (!empty()) {
-      length_--;
-      bytes_.erase(length_);
-    }
+	if (!empty()) {
+	  length_--;
+	  bytes_.erase(length_);
+	}
   }
 
   /// Element anfügen
@@ -288,10 +288,10 @@ public:
   /// // array = {1, 2}
   /// \endcode
   void push_back(const value_type& val) {
-    if (!is_full()) {
-      bytes_.emplace(length_, val);
-      length_++;
-    }
+	if (!is_full()) {
+	  bytes_.emplace(length_, val);
+	  length_++;
+	}
   }
 
   /// Element für Konstructor anfügen.
@@ -321,10 +321,10 @@ public:
   /// \param args Argumente für einen Konstruktor von Typ \a T um Element zu erstellen
   template<class... Args> _always_inline
   void emplace_back(Args&&... args) {
-    if (!is_full()) {
-      bytes_.emplace(length_, std::forward<Args>(args)...);
-      length_++;
-    }
+	if (!is_full()) {
+	  bytes_.emplace(length_, std::forward<Args>(args)...);
+	  length_++;
+	}
   }
 
   /// Fügt Element \a val vorne an
@@ -333,7 +333,7 @@ public:
   /// alle folgenden Elemente verschoben werden, was entsprechend Zeit kostet.
   /// \param val Element was am Anfang des Arrays hinzugefügt werden soll.
   void push_front(const value_type& val) {
-    insert(begin(), val);
+	insert(begin(), val);
   }
 
   /// Fügt Element vorne an
@@ -343,10 +343,10 @@ public:
   /// \param args Argumente für einen Konstruktor von Typ \a T um Element zu erstellen
   template<class... Args> _always_inline
   void emplace_front(Args&&... args) {
-    if (prepare_for_insert(begin())) {
-      length_++;
-      bytes_.emplace(begin(), std::forward<Args>(args)...);
-    }
+	if (prepare_for_insert(begin())) {
+	  length_++;
+	  bytes_.emplace(begin(), std::forward<Args>(args)...);
+	}
   }
 
   /// Alle Elemente entfernen
@@ -361,8 +361,8 @@ public:
   /// array.erase(array.begin(), array.end())
   /// \endcode
   void clear() {
-    bytes_.erase(begin(), end());
-    length_ = 0;
+	bytes_.erase(begin(), end());
+	length_ = 0;
   }
 
   /// Element an beliebiger Position einfügen
@@ -380,10 +380,10 @@ public:
   /// \param position Iterator auf Element in Array an das Element eingefügt werden soll
   /// \param val einzufügendes Element
   void insert(iterator position, value_type val) {
-    if (prepare_for_insert(position)) {
-      length_++;
-      bytes_.emplace(position, std::move(val));
-    }
+	if (prepare_for_insert(position)) {
+	  length_++;
+	  bytes_.emplace(position, std::move(val));
+	}
   }
 
   /// Element an beliebiger Position einfügen
@@ -411,18 +411,18 @@ public:
   /// \param args Argumente für einen Konstruktor von Typ \a T um Element zu erstellen
   template<class... Args> _always_inline
   void emplace(iterator position, Args&&... args) {
-    if (prepare_for_insert(position)) {
-      length_++;
-      bytes_.emplace(position, std::forward<Args>(args)...);
-    }
+	if (prepare_for_insert(position)) {
+	  length_++;
+	  bytes_.emplace(position, std::forward<Args>(args)...);
+	}
   }
 
   /// Element entfernen
   /// \param position Iterator auf Element, was entfernt werden soll
   void erase(iterator position) {
-    std::move(position + 1, end(), position);
-    length_--;
-    bytes_.erase(end());
+	std::move(position + 1, end(), position);
+	length_--;
+	bytes_.erase(end());
   }
 
   /// Elemente die entfernt werden sollen
@@ -441,9 +441,9 @@ public:
   /// \param first Iterator auf erstes zu entfernendes Element
   /// \param last Iterator erstes nicht zu entfernendes Element
   void erase(iterator first, iterator last) {
-    iterator new_end = std::move(last, end(), first);
-    bytes_.erase(new_end, end());
-    length_ = new_end - begin();
+	iterator new_end = std::move(last, end(), first);
+	bytes_.erase(new_end, end());
+	length_ = new_end - begin();
   }
 
 private:
@@ -455,17 +455,17 @@ private:
 
   bool prepare_for_insert(iterator position) {
 #ifdef NDEBUG
-    return _ArrayBufferHelper<T>::prepare_for_insert(position, end());
+	return _ArrayBufferHelper<T>::prepare_for_insert(position, end());
 #else
-    return _ArrayBufferHelper<T>::prepare_for_insert(position, end(), const_iterator(&bytes_[N]));
+	return _ArrayBufferHelper<T>::prepare_for_insert(position, end(), const_iterator(&bytes_[N]));
 #endif
   }
 
   bool is_full() {
 #ifdef NDEBUG
-    return false;
+	return false;
 #else
-    return _ArrayBufferHelper<T>::is_full(cend(), const_iterator(&bytes_[N]));
+	return _ArrayBufferHelper<T>::is_full(cend(), const_iterator(&bytes_[N]));
 #endif
   }
 
@@ -486,8 +486,8 @@ bool _ArrayBufferHelper<T>::prepare_for_insert(iterator position, iterator end) 
 template<typename T>
 bool _ArrayBufferHelper<T>::prepare_for_insert(iterator position, iterator end, const_iterator max_end) {
   if (end == max_end) {
-    turag_error("ArrayBuffer overflow!");
-    return false;
+	turag_error("ArrayBuffer overflow!");
+	return false;
   }
 
   std::move_backward(position, end, end + 1);
@@ -499,12 +499,27 @@ bool _ArrayBufferHelper<T>::prepare_for_insert(iterator position, iterator end, 
 template<typename T>
 bool _ArrayBufferHelper<T>::is_full(const_iterator end, const_iterator max_end) {
   if (end == max_end) {
-    turag_error("ArrayBuffer overflow!");
-    return true;
+	turag_error("ArrayBuffer overflow!");
+	return true;
   }
   return false;
 }
 #endif
+
+template <typename T, size_t N>
+static ArrayBuffer<size_t, N> sort_indexes(const ArrayBuffer<T, N> &v) {
+
+  // initialize original index locations
+  ArrayBuffer<size_t, N> idx;
+  for (size_t i = 0; i != v.size(); ++i) idx[i] = i;
+
+  // sort indexes based on comparing values in v
+  std::sort(idx.begin(), idx.end(),
+	   [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
+
+  return idx;
+}
+
 
 #endif // DOXYGEN
 
