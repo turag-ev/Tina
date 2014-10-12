76f105bc02e67df65534b73e09cce3af4384d34a
Martin Oemus <martin@oemus.net>
2014-10-12 19:19:47 +0200

    [tina] kleinen Bug in feldbus-device behoben

--- tina/tina++/feldbus/host/device.h
+++ tina/tina++/feldbus/host/device.h
@@ -152,7 +152,7 @@ public:
 	/*!
 	 * \brief transceive
 	 */
-	template<typename T, typename U> _always_inline
+    template<typename T> _always_inline
 	bool transceive(Broadcast<T>& transmit) {
 		transmit.address = TURAG_FELDBUS_BROADCAST_ADDR;
 		return transceive(static_cast<uint8_t*>(static_cast<void*>(std::addressof(transmit))),
