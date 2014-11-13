ce6f9c6ba79f3f57800da822d73f37d22477376e
Florian Voelker <florian_voelker@turag.de>
2014-11-13 22:45:52 +0100

    [Console] Feldbusklasse komplett eingebunden. Rechtschreibfehler in farbsensor behoben

--- tina/tina++/feldbus/host/bootloader.h
+++ tina/tina++/feldbus/host/bootloader.h
@@ -21,17 +21,13 @@ namespace Feldbus {
 class Bootloader : public TURAG::Feldbus::Device {
 
 public:
-    Bootloader(TURAG::Feldbus::Device *dev_);
+    Bootloader(const char* name_, int address, ChecksumType type = TURAG_FELDBUS_DEVICE_CONFIG_STANDARD_CHECKSUM_TYPE) :
+        Device(name_, address, type) {}
+
     bool transceiveBoot (uint8_t *transmit, int transmit_length, uint8_t *receive, int receive_length);
-
-protected:
-    
-
-private:
-
 };
 
 } // namespace Feldbus
 } // namespace TURAG
 
-#endif /* TURAGFELDBUSASEB_H_ */
+#endif /* TURAGFELDBUSBOOTLOADER_H_ */
