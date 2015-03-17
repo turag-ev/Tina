1a3ed14e6df6431808e93a6172d208e110bfff36
Richard Liebscher <richard_liebscher@turag.de>
2015-03-17 01:01:20 +0100

    [Sim] Fehler in CDLayer behoben. alte Schnittstelle für GraphicsLayer wird wieder unterstützt.

--- tina/tina++/statemachine/eventqueue.h
+++ tina/tina++/statemachine/eventqueue.h
@@ -360,42 +360,9 @@ public:
    */
   void push(const EventClass* event_class, EventArg params = 0, EventMethod method = nullptr);
 
-  template<typename T, REQUIRES(!std::is_integral<T>)> _always_inline
-  void push(const EventClass* event_class, T param, EventMethod method = nullptr) {
-    push(event_class, pack<EventArg>(param), method);
-  }
-
-/*  // for compatibility with old code
-  template<EventId id>
-  void push(EventArg param = 0, EventMethod method = nullptr) {
-    push(&UnnamedEventClass<id>::event_class, param, method);
-  }
-  // for compatibility with old code
-  template<EventId id, typename T, REQUIRES(!std::is_integral<T>)> _always_inline
-  void push(T param, EventMethod method = nullptr) {
-    push(&UnnamedEventClass<id>::event_class, pack<EventArg>(param), method);
-  }*/
-
   /// Push an new event to the front of the event processing loop
   /** Paramters the same as in push */
   void pushToFront(const EventClass* event_class, EventArg param = 0, EventMethod method = nullptr);
-
-  template<typename T, REQUIRES(!std::is_integral<T>)> _always_inline
-  void pushToFront(const EventClass* event_class, T param, EventMethod method = nullptr) {
-    pushToFront(event_class, pack<EventArg>(param), method);
-  }
-
-/*  // for compatibility with old code
-  template<EventId id>
-  void pushToFront(EventArg param = 0, EventMethod method = nullptr) {
-    pushToFront(&UnnamedEventClass<id>::event_class, param, method);
-  }
-
-  // for compatibility with old code
-  template<EventId id, typename T, REQUIRES(!std::is_integral<T>)> _always_inline
-  void pushToFront(T param, EventMethod method = nullptr) {
-    pushToFront(&UnnamedEventClass<id>::event_class, pack<EventArg>(param), method);
-  }*/
 
   /// Process event in a number of kernel ticks
   /**
@@ -407,23 +374,6 @@ public:
    *               event is given to the main action event function.
    */
   void pushTimedelayed(SystemTime ticks, const EventClass* event_class, EventArg param = 0, EventMethod method = nullptr);
-
-  template<typename T, REQUIRES(!std::is_integral<T>)> _always_inline
-  void pushTimedelayed(SystemTime ticks, const EventClass* event_class, T param, EventMethod method = nullptr) {
-    pushTimedelayed(ticks, event_class, pack<EventArg>(param), method);
-  }
-
-/*  // for compatibility with old code
-  template<EventId id>
-  void pushTimedelayed(SystemTime ticks, EventArg param = 0, EventMethod method = nullptr) {
-    pushTimedelayed(ticks, &UnnamedEventClass<id>::event_class, param, method);
-  }
-
-  // for compatibility with old code
-  template<EventId id, typename T, REQUIRES(!std::is_integral<T>)> _always_inline
-  void pushTimedelayed(SystemTime ticks, T param, EventMethod method = nullptr) {
-    pushTimedelayed(ticks, &UnnamedEventClass<id>::event_class, pack<EventArg>(param), method);
-  }*/
 
   /// Remove all events from timedelayed event queue with that id.
   /**
@@ -487,7 +437,7 @@ public:
   static const size_t timequeue_size = 32;
 
   /// maximale Zeit zwischen zwei Aufrufen der tick-Funktion (Parameter von \ref main Funktion)
-  static constexpr SystemTime max_tick_time = 100_ms;
+  static constexpr SystemTime max_tick_time = SystemTime::fromMsec(100);
 
 private:
   /// Mutex für thread-sichere Verarbeitung
