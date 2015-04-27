8d6cae8b128b39a829c5891f3ece34c48cd57e7e
Richard Liebscher <richard_liebscher@turag.de>
2015-04-27 19:44:33 +0200

    [SC] Strategy für Kollisionserkennungstests hinzugefügt. ...
    Simulation compiliert wieder.
    Scene-Fenster ist jetzt oberstes Fenster.

---
+++ tina/tina++/geometry/spline.cpp
@@ -0,0 +1,9 @@
+#include "spline.h"
+
+namespace TURAG {
+
+constexpr Length Spline::spline_iteration_distance;
+constexpr float Spline::spline_form_factor;
+constexpr float Spline::mc_no_angle;
+
+} // namespace TURAG
