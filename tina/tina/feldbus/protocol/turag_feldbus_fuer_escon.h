bf8773da8792e23c714110aafd633bb22b726e89
Lukas Müller <lukas.mueller4@mailbox.tu-dresden.de>
2016-04-13 21:59:04 +0200

    [Boa/LMC]RS485-Kommunikations Protokoll, LMC RS485 Code

--- tina/tina/feldbus/protocol/turag_feldbus_fuer_escon.h
+++ tina/tina/feldbus/protocol/turag_feldbus_fuer_escon.h
@@ -3,4 +3,23 @@
 
 #include "turag_feldbus_bus_protokoll.h"
 
+//command bytes
+#define TURAG_FELDBUS_ESCON_SET_RPM 0x01
+
+//command structures
+struct TuragEsconSetRPM_t {
+    int16_t value[2];
+};
+
+//response structures
+struct TuragEsconStatus_t {
+    uint8_t status;
+    int16_t currentRPM[2];
+    int16_t measuredCurrent[2];
+};
+
+//status bytes
+#define TURAG_FELDBUS_ESCON_READY 0
+#define TURAG_FELDBUS_ESCON_FAILURE 1
+
 #endif
