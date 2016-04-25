e4a8a320a24e7cc57607f3f5659b3e7b2ef7c628
Martin Oemus <martin@oemus.net>
2016-04-25 05:19:30 +0200

    [tina] bisschen Doku

--- tina/tina++/feldbus/host/servo.h
+++ tina/tina++/feldbus/host/servo.h
@@ -27,29 +27,79 @@ namespace Feldbus {
 class ServoBase : public TURAG::Feldbus::Aktor {
 public:
 	/**
-	 * @brief Liest den aktuellen Winkel.
+	 * @brief Liest den aktuellen Winkel in mRad.
 	 * @param[out] angle Puffer für den Winkel.
 	 * @return True bei Erfolg, ansonsten false.
 	 */
     bool getCurrentAngle(float* angle) { return getValue(RS485_STELLANTRIEBE_KEY_CURRENT_ANGLE, angle); }
 
 	/**
-	 * @brief Setzt den Sollwinkel.
-	 * @param angle Sollwinkel.
-	 * @return
+	 * @brief Gibt den Sollwinkel in mRad zurück.
+	 * @param[in] angle Sollwinkel.
+	 * @return True bei Erfolg, ansonsten false.
+	 *
+	 * Verursacht keine Buslast.
 	 */
     bool getDesiredAngle(float* angle) { return getValue(RS485_STELLANTRIEBE_KEY_DESIRED_ANGLE, angle); }
-    virtual bool setDesiredAngle(float angle) { return setValue(RS485_STELLANTRIEBE_KEY_DESIRED_ANGLE, angle); }
+	/**
+	 * @brief Setzt den Sollwinkel.
+	 * @param[in] angle Sollwinkel in mRad.
+	 * @return True bei Erfolg, ansonsten false.
+	 * @post Aktiviert die Regelung.
+	 */
+	virtual bool setDesiredAngle(float angle) { return setValue(RS485_STELLANTRIEBE_KEY_DESIRED_ANGLE, angle); }
 
+	/**
+	 * @brief Gibt den maximalen Winkel in mRad zurück.
+	 * @param[out] angle Maximaler Winkel.
+	 * @return True bei Erfolg, ansonsten false.
+	 *
+	 * Verursacht keine Buslast.
+	 */
     bool getMaxAngle(float* angle) { return getValue(RS485_STELLANTRIEBE_KEY_MAX_ANGLE, angle); }
+	/**
+	 * @brief Setzt den maximalen Winkel in mRad.
+	 * @param[in] angle Maximaler Winkel.
+	 * @return True bei Erfolg, ansonsten false.
+	 */
     bool setMaxAngle(float angle) { return setValue(RS485_STELLANTRIEBE_KEY_MAX_ANGLE, angle); }
 
-    bool getMinAngle(float* angle) { return getValue(RS485_STELLANTRIEBE_KEY_MIN_ANGLE, angle); }
-    bool setMinAngle(float angle) { return setValue(RS485_STELLANTRIEBE_KEY_MIN_ANGLE, angle); }
+	/**
+	 * @brief Gibt den minimalen Winkel in mRad zurück.
+	 * @param[out] angle Minimaler Winkel.
+	 * @return True bei Erfolg, ansonsten false.
+	 *
+	 * Verursacht keine Buslast.
+	 */
+	bool getMinAngle(float* angle) { return getValue(RS485_STELLANTRIEBE_KEY_MIN_ANGLE, angle); }
+	/**
+	 * @brief Setzt den minimalen Winkel in mRad.
+	 * @param[in] angle Minimaler Winkel.
+	 * @return True bei Erfolg, ansonsten false.
+	 */
+	bool setMinAngle(float angle) { return setValue(RS485_STELLANTRIEBE_KEY_MIN_ANGLE, angle); }
 
+	/**
+	 * @brief Gibt die aktuelle Geschwindigkeit in mRad/s zurück.
+	 * @param[out] velocity Geschwindigkeit.
+	 * @return True bei Erfolg, ansonsten false.
+	 */
     bool getCurrentVelocity(float* velocity) { return getValue(RS485_STELLANTRIEBE_KEY_CURRENT_VELOCITY, velocity); }
 
+	/**
+	 * @brief Gibt die Sollgeschwindigkeit in mRad/s zurück.
+	 * @param velocity Sollgeschwindigkeit.
+	 * @return True bei Erfolg, ansonsten false.
+	 *
+	 * Verursacht keine Buslast.
+	 */
     bool getDesiredVelocity(float* velocity) { return getValue(RS485_STELLANTRIEBE_KEY_DESIRED_VELOCITY, velocity); }
+	/**
+	 * @brief Setzt die Sollgeschwindigkeit in mRad/s.
+	 * @param velocity Sollgeschwindigkeit.
+	 * @return True bei Erfolg, ansonsten false.
+	 * @post Aktiviert die Regelung.
+	 */
     virtual bool setDesiredVelocity(float velocity) { return setValue(RS485_STELLANTRIEBE_KEY_DESIRED_VELOCITY, velocity); }
 
     bool getMaxVelocity(float* velocity) { return getValue(RS485_STELLANTRIEBE_KEY_MAX_VELOCITY, velocity); }
@@ -58,7 +108,13 @@ public:
     bool getCurrentCurrent(float* current) { return getValue(RS485_STELLANTRIEBE_KEY_CURRENT_CURRENT, current); }
 
     bool getDesiredCurrent(float* current) { return getValue(RS485_STELLANTRIEBE_KEY_DESIRED_CURRENT, current); }
-    bool setDesiredCurrent(float current) { return setValue(RS485_STELLANTRIEBE_KEY_DESIRED_CURRENT, current); }
+	/**
+	 * @brief Setzt den Sollstrom in mA.
+	 * @param pwm Sollstrom.
+	 * @return True bei Erfolg, ansonsten false.
+	 * @post Aktiviert die Regelung.
+	 */
+	bool setDesiredCurrent(float current) { return setValue(RS485_STELLANTRIEBE_KEY_DESIRED_CURRENT, current); }
 
     bool getMaxCurrent(float* current) { return getValue(RS485_STELLANTRIEBE_KEY_MAX_CURRENT, current); }
     bool setMaxCurrent(float current) { return setValue(RS485_STELLANTRIEBE_KEY_MAX_CURRENT, current); }
@@ -71,15 +127,35 @@ public:
     bool getMaxPwm(float* pwm) { return getValue(RS485_STELLANTRIEBE_KEY_MAX_PWM, pwm); }
     bool setMaxPwm(float pwm) { return setValue(RS485_STELLANTRIEBE_KEY_MAX_PWM, pwm); }
 
+	/**
+	 * @brief Gibt die aktuelle Versorgungsspannung in Volt zurück.
+	 * @param voltage Puffer für die Versorgungsspannung.
+	 * @return True bei Erfolg, ansonsten false.
+	 */
     bool getVoltage(float* voltage) { return getValue(RS485_STELLANTRIEBE_KEY_VOLTAGE, voltage); }
 
+	/**
+	 * @brief Aktiviert die Regelung.
+	 * @return True bei Erfolg, ansonsten false.
+	 *
+	 * Die Regelung wird durch die die drei Sollwert-Stellbefehle setDesiredCurrent(), setDesiredVelocity()
+	 * und setDesiredAngle() automatisch aktiviert.
+	 */
     virtual bool enableControl() {
         return setValue(RS485_STELLANTRIEBE_KEY_CONTROL_STATE, static_cast<int32_t>(RS485_STELLANTRIEBE_CONTROL_STATE_ENABLE));
     }
+	/**
+	 * @brief Deaktiviert die Regelung.
+	 * @return True bei Erfolg, ansonsten false.
+	 */
     virtual bool disableControl() {
         return setValue(RS485_STELLANTRIEBE_KEY_CONTROL_STATE, static_cast<int32_t>(RS485_STELLANTRIEBE_CONTROL_STATE_DISABLE));
     }
 
+	/**
+	 * @brief Hält den Antrieb an und regelt auf die aktuelle Position.
+	 * @return True bei Erfolg, ansonsten false.
+	 */
     virtual bool setHold(void);
 
     /** Status request
