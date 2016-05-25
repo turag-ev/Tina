980579d0e62f561ecf3c664ef33e9de44890cc14
Richard Liebscher <richard_liebscher@turag.de>
2016-05-25 13:00:48 +0200

    [tina] Spline Implementation aufgeräumt (um 2% kleiners Binary für LMC)

--- tina/tina++/geometry/spline.cpp
+++ tina/tina++/geometry/spline.cpp
@@ -2,8 +2,246 @@
 
 namespace TURAG {
 
-constexpr Length Spline::spline_iteration_distance;
-constexpr float Spline::spline_form_factor;
-constexpr float Spline::mc_no_angle;
+// Spline
+
+constexpr Length SplineBase::spline_iteration_distance;
+constexpr float SplineBase::spline_form_factor;
+constexpr Angle SplineBase::mc_no_angle;
+
+void SplineBase::calc_spline_angle(Pose *p, unsigned i, unsigned pn, int dir)
+{
+	if (i < pn && isAngleDontCare(p[i].phi)) {
+		if (i > 0) {
+			p[i].phi = angle_between(p[i-1], p[i+1]);
+		} else {
+			p[i].phi = angle_between(p[i], p[i+1]);
+		}
+
+		if (dir == -1) {
+			p[i].turn(180 * Units::deg); // turn angle for drive backwards
+		}
+	}
+
+	if ((i+1) < pn && isAngleDontCare(p[i+1].phi)) {
+		if (i < pn-2){
+			p[i+1].phi = angle_between(p[i], p[i+2]);
+		} else {
+			p[i+1].phi = angle_between(p[i], p[i+1]);
+		}
+
+		if (dir == -1) {
+			p[i+1].turn(180 * Units::deg); // turn angle for drive backwards
+		}
+	}
+}
+
+// SplineOrder<X>
+
+template<std::size_t order>
+bool SplineOrder<order>::calculate(Pose *poses, unsigned pose_index,
+								   unsigned pose_count, int direction)
+{
+	drive_direction = direction;
+
+	calc_spline_angle(poses, pose_index, pose_count, direction);
+
+	calc_spline(poses, pose_index);
+
+	return calculate_parameters();
+}
+
+template<std::size_t order>
+Pose SplineOrder<order>::getPoseStep(float t)
+{
+	Pose p;
+
+	p.x = c.x.val(t) * Units::mm;
+	p.y = c.y.val(t) * Units::mm;
+
+	// calculate angle from tangent
+	float dx = c.x.template val<1>(t);
+	float dy = c.y.template val<1>(t);
+
+	p.phi = atan2f(dy, dx) * Units::rad;
+
+	// for backwards driven splines the angles phi correspond to the robot's angle
+	// but to get the desired spline form without curls we need to pretend it's driven forward
+	if (drive_direction == -1) {
+		p.turn(180 * Units::deg);
+	}
+
+	return p;
+}
+
+template<std::size_t order>
+float SplineOrder<order>::getBendingStep(float t)
+{
+	// Wert der 1. Ableitung an Stelle t
+	float dx = c.x.template val<1>(t);
+	float dy = c.y.template val<1>(t);
+
+	// Wert der 2. Ableitung an Stelle t
+	float ddx = c.x.template val<2>(t);
+	float ddy = c.y.template val<2>(t);
+
+	float bending = (dx * ddy - ddx * dy) / cbcf(hypotf(dx, dy));
+	return bending;
+}
+
+// calc spline catmullrom
+template<>
+void SplineOrder<3>::calc_spline(Pose *p, unsigned pi)
+{
+	// see getPoseStep()
+	Angle aoff = 0 * Units::deg;
+	if (drive_direction == -1) {
+		aoff = 180 * Units::deg;
+	}
+
+	// spline form depends from distance between p[i] and p[i+1]
+	float d12 = distance(p[pi], p[pi+1]).to(Units::mm);
+	float k = spline_form_factor * d12;
+
+	float vx[4] = { k * cos(p[pi].phi + aoff),
+					k * cos(p[pi + 1].phi + aoff),
+					p[pi].x.to(Units::mm),
+					p[pi + 1].x.to(Units::mm) };
+	float vy[4] = { k * sin(p[pi].phi + aoff),
+					k * sin(p[pi + 1].phi + aoff),
+					p[pi].y.to(Units::mm),
+					p[pi + 1].y.to(Units::mm) };
+
+	/* invertierte hermitesche kubische Matrix (siehe Wikipedia)
+	 *
+	 * errechnet Splineparameter anhand gegebener Randwerte:
+	 * - Position p
+	 * - Tangente bzw. Winkel m
+	 *
+	 *  v = [m0 m1 p0 p1]' siehe oben
+	 */
+	static constexpr float m[4][4] =
+				   {{ 1, 1, 2,-2},
+					{-2,-1,-3, 3},
+					{ 1, 0, 0, 0},
+					{ 0, 0, 1, 0}};
+
+	/*
+	 * Matrixmultiplikation c = M * v
+	 */
+	for (int i = 0; i < 4; i++) {
+		c.x.c[i] = 0.0f;
+		c.y.c[i] = 0.0f;
+
+		for (int j = 0; j < 4; j++) {
+			c.x.c[i] += m[i][j] * vx[j];
+			c.y.c[i] += m[i][j] * vy[j];
+		}
+	}
+
+	direct_dist = d12 * Units::mm;
+}
+
+// calc spline hermite
+template<>
+void SplineOrder<5>::calc_spline(Pose *p, unsigned pi)
+{
+	// see getPoseStep()
+	Angle aoff = 0 * Units::deg;
+	if (drive_direction == -1) {
+		aoff = 180 * Units::deg;
+	}
+
+	// spline form depends from distance between p[i] and p[i+1]
+	float d12 = distance(p[pi], p[pi+1]).to(Units::mm);
+	float k = spline_form_factor * d12;
+
+	float vx[4] = { p[pi].x.to(Units::mm),
+					k * cos(p[pi].phi + aoff),
+					p[pi + 1].x.to(Units::mm),
+					k * cos(p[pi + 1].phi + aoff) };
+	float vy[4] = { p[pi].y.to(Units::mm),
+					k * sin(p[pi].phi + aoff),
+					p[pi + 1].y.to(Units::mm),
+					k * sin(p[pi + 1].phi + aoff) };
+
+	static constexpr float m[3][4] =
+				   {{ -6,-3,  6,-3},
+					{ 15, 8,-15, 7},
+					{-10,-6, 10,-4}};
+
+	c.x.c[3] = 0.0f;
+	c.y.c[3] = 0.0f;
+	c.x.c[4] = vx[1];
+	c.y.c[4] = vy[1];
+	c.x.c[5] = vx[0];
+	c.y.c[5] = vy[0];
+
+	for (int i = 0; i < 3; i++) {
+		c.x.c[i] = 0.0f;
+		c.y.c[i] = 0.0f;
+
+		for (int j = 0; j < 4; j++) {
+			c.x.c[i] += m[i][j] * vx[j];
+			c.y.c[i] += m[i][j] * vy[j];
+
+		}
+	}
+
+	direct_dist = d12 * Units::mm;
+}
+
+template<std::size_t order>
+bool SplineOrder<order>::calculate_parameters()
+{
+	float t, dx, dy, ddx, ddy;
+
+	float delta_t = spline_iteration_distance / direct_dist;
+	Point p_old = {c.x.getNode(order) * Units::mm, c.y.getNode(order) * Units::mm};
+
+	// max Krümmung
+	kappa_max = 0.f;
+
+	// Bogenlänge
+	length = 0 * Units::mm;
+
+	// Abtastung des Polynoms
+	for (t = delta_t; t <= 1.0f + delta_t; t = t + delta_t) {
+		// Wert an Stelle t
+		Point p = {c.x.val(t) * Units::mm, c.y.val(t) * Units::mm};
+
+		// Gesamtlänge += Abschnittslänge
+		length += distance(p_old, p);
+
+		// merken der Werte für Abstandsberechnung im nächsten Iterationschritt
+		p_old = p;
+
+		// Wert der 1. Ableitung an Stelle t
+		dx = c.x.template val<1>(t);
+		dy = c.y.template val<1>(t);
+
+		// Wert der 2. Ableitung an Stelle t
+		ddx = c.x.template val<2>(t);
+		ddy = c.y.template val<2>(t);
+
+		// max Krümmung bestimmen
+		kappa_max = fmaxf(kappa_max, fabsf((dx * ddy - ddx * dy)) / cbcf(hypotf(dx, dy)));
+	}
+
+	// Begrenzung...
+
+	//...der Bogenlänge: min. spline_iteration_distance
+	length = max(length, spline_iteration_distance);
+
+	//...der Krümmung: min. 1e-10f
+	kappa_max = fmaxf(kappa_max, 1e-10f);
+
+	return true;
+}
+
+
+// explicit instantiation of SplineOrder<X> class
+template class SplineOrder<3>;
+template class SplineOrder<5>;
+
 
 } // namespace TURAG
--- tina/tina++/geometry/spline.h
+++ tina/tina++/geometry/spline.h
@@ -14,9 +14,15 @@ class Polynomial
 {
 	static_assert(order == 3 || order == 5, "only order 3 and 5 implemented");
 public:
+
+	enum : std::size_t { Order = order };
+
+	// value & derivation
+
 	template<std::size_t derivation = 0>
 	inline float val(float t) const { return 0.f; }
-	float val(float t, unsigned derivation) const;
+
+	// item access
 
 	constexpr float getNode(unsigned index) const {
         return c[index];
@@ -29,6 +35,18 @@ public:
 	float& operator[](unsigned index) {
 		return c[index];
 	}
+
+	// range
+
+	using iterator = float*;
+	using const_iterator = const float*;
+
+	std::size_t size() const { return order + 1; }
+
+	iterator begin() { return c; }
+	const_iterator begin() const { return c; }
+	iterator end() { return c + order + 1; }
+	const_iterator end() const { return c + order + 1; }
 
     // polynomial coefficients
     float c[order+1];
@@ -50,22 +68,6 @@ template<> template<>
 inline float Polynomial<3>::val<3>(float) const {
 	return 6.0f * c[0];
 }
-template<>
-inline float Polynomial<3>::val(float t, unsigned derivation) const {
-	switch (derivation) {
-	case 0:
-		return ((c[0] * t + c[1]) * t + c[2]) * t + c[3];
-	case 1:
-		return (3.0f * c[0] * t + 2.0f * c[1]) * t + c[2];
-	case 2:
-		return 6.0f * c[0] * t + 2.0f * c[1];
-	case 3:
-		return 6.0f * c[0];
-	default:
-		return 0.0f;
-	}
-}
-
 
 template<> template<>
 inline float Polynomial<5>::val<0>(float t) const {
@@ -87,32 +89,11 @@ inline float Polynomial<5>::val<3>(float t) const {
 }
 template<> template<>
 inline float Polynomial<5>::val<4>(float t) const {
-	return 60.0f * c[0] * t + 24.0f * c[1];
+	return 120.0f * c[0] * t + 24.0f * c[1];
 }
 template<> template<>
 inline float Polynomial<5>::val<5>(float) const {
-	return 60.0f * c[0];
-}
-template<>
-inline float Polynomial<5>::val(float t, unsigned derivation) const {
-	switch (derivation) {
-	case 0:
-		return ((((c[0] * t + c[1]) * t + c[2]) * t + c[3]) * t + c[4]) * t + c[5];
-	case 1:
-		return (((5.0f * c[0] * t + 4.0f * c[1]) * t + 3.0f * c[2]) * t +
-				2.0f * c[3]) * t + c[4];
-	case 2:
-		return ((20.0f * c[0] * t + 12.0f * c[1]) * t + 6.0f * c[2]) * t +
-				2.0f * c[3];
-	case 3:
-		return (60.0f * c[0] * t + 24.0f * c[1]) * t + 6.0f * c[2];
-	case 4:
-		return 60.0f * c[0] * t + 24.0f * c[1];
-	case 5:
-		return 60.0f * c[0];
-	default:
-		return 0.0f;
-	}
+	return 120.0f * c[0];
 }
 
 /**
@@ -122,308 +103,66 @@ template<std::size_t order>
 class Polynomial2D
 {
 public:
+	enum : std::size_t { Order = order };
+
     // polynomial coefficients
     Polynomial<order> x, y;
 };
 
 /**
- * @brief Spline interface for all types of splines (Catmull-Rom, Hermite, Bezier, ...)
+ * @brief Base class for splines
+ * @details intern class for order independend code
  */
-class Spline
+class SplineBase
 {
-public:
-    virtual bool calculate(Pose *poses, unsigned pose_index, unsigned pose_count, int direction) = 0;
-    virtual Pose getPoseStep(float t) = 0;
-    virtual float getBendingStep(float t) = 0;
-    virtual unsigned getOrder() const = 0;
-    virtual Length getLength() const = 0;
-    virtual float getKappaMax() const = 0;
-    virtual Length getDirectDist() const = 0;
-
 protected:
     static constexpr Length spline_iteration_distance = 50 * Units::mm;
     static constexpr float spline_form_factor = 1.0f;
-    static constexpr float mc_no_angle = 4.f;
+	static constexpr Angle mc_no_angle = 4.f * Units::rad;
 
 	static inline bool isAngleDontCare(Units::Angle a)
 	{
-		return (fabsf(a.to(Units::rad) - mc_no_angle) < 0.001f) || std::isnan(a.to(Units::rad));
+		return (abs(a - mc_no_angle) < 0.001f * Units::rad) || isnan(a);
 	}
 
 	/**
 	 * @brief calc_spline_angle calculate missing angle of spline point
 	 */
-	static void calc_spline_angle(Pose *p, unsigned i, unsigned pn, int dir) {
-		if (i < pn && isAngleDontCare(p[i].phi)) {
-			if (i > 0) {
-				p[i].phi = angle_between(p[i-1], p[i+1]);
-			} else {
-				p[i].phi = angle_between(p[i], p[i+1]);
-			}
+	static void calc_spline_angle(Pose *p, unsigned i, unsigned pn, int dir);
 
-			if (dir == -1) {
-				p[i].turn(180 * Units::deg); // turn angle for drive backwards
-			}
-		}
-
-		if ((i+1) < pn && isAngleDontCare(p[i+1].phi)) {
-			if (i < pn-2){
-				p[i+1].phi = angle_between(p[i], p[i+2]);
-			} else {
-				p[i+1].phi = angle_between(p[i], p[i+1]);
-			}
-
-			if (dir == -1) {
-				p[i+1].turn(180 * Units::deg); // turn angle for drive backwards
-			}
-		}
-	}
+	Length direct_dist;
+	Length length;
+	float kappa_max = 0.f;
+	int drive_direction = 0;
 };
 
 /**
  * @brief SplineOrder Spline of a certain order
- * @todo move impl to cpp-file and remove code bloat
  */
 template<std::size_t order>
-class SplineOrder final :
-        public Spline
+class SplineOrder : private SplineBase
 {
 	static_assert(order == 3 || order == 5, "only order 3 and 5 implemented");
 public:
-	bool calculate(Pose *poses, unsigned pose_index, unsigned pose_count, int direction) override {
-        drive_direction = direction;
 
-        if (order == 3) {
-            calc_spline_angle(poses, pose_index, pose_count, direction);
-            return calc_spline_catmullrom(poses, pose_index);
-        } else if (order == 5) {
-            calc_spline_angle(poses, pose_index, pose_count, direction);
-            return calc_spline_hermite(poses, pose_index);
-        }
+	enum : std::size_t { Order = order };
 
-        return false;
-    }
+	bool calculate(Pose *poses, unsigned pose_index, unsigned pose_count, int direction);
 
-	constexpr unsigned getOrder() const override {
-        return order;
-    }
+	constexpr Length getLength() const     { return length; }
+	constexpr float getKappaMax() const    { return kappa_max; }
+	constexpr Length getDirectDist() const { return direct_dist; }
+	constexpr unsigned getOrder() const    { return order; }
 
-	constexpr Length getLength() const override {
-        return length;
-    }
-
-	constexpr float getKappaMax() const override {
-        return kappa_max;
-    }
-
-	constexpr Length getDirectDist() const override {
-        return direct_dist;
-    }
-
-	Pose getPoseStep(float t) override
-    {
-        Pose p;
-
-		p.x = c.x.val(t) * Units::mm;
-		p.y = c.y.val(t) * Units::mm;
-
-        // calculate angle from tangent
-		float dx = c.x.template val<1>(t);
-		float dy = c.y.template val<1>(t);
-
-        p.phi = atan2f(dy, dx) * Units::rad;
-
-        // for backwards driven splines the angles phi correspond to the robot's angle
-        // but to get the desired spline form without curls we need to pretend it's driven forward
-        if (drive_direction == -1) {
-            p.turn(180 * Units::deg);
-        }
-
-        return p;
-    }
-
-	float getBendingStep(float t) override
-    {
-		float dx = c.x.template val<1>(t);
-		float dy = c.y.template val<1>(t);
-
-		float ddx = c.x.template val<2>(t);
-		float ddy = c.y.template val<2>(t);
-
-        float bending = (dx * ddy - ddx * dy) / cbcf(hypotf(dx, dy));
-        return bending;
-    }
+	Pose getPoseStep(float t);
+	float getBendingStep(float t);
 
 private:
 
-	bool calc_spline_catmullrom(Pose *poses, unsigned pose_index)
-    {
-        //static_assert(order == 3, "wrong spline order for this call");
-
-        Pose *p = poses;
-		unsigned i = pose_index;
-
-        // see getPoseStep()
-        Angle aoff = 0 * Units::deg;
-        if (drive_direction == -1) {
-            aoff = 180 * Units::deg;
-        }
-
-        // spline form depends from distance between p[i] and p[i+1]
-		float d12 = distance(p[i], p[i+1]).to(Units::mm);
-        float k = spline_form_factor * d12;
-
-		float vx[4] = { k * cos(p[i].phi + aoff),
-						k * cos(p[i + 1].phi + aoff),
-						p[i].x.to(Units::mm),
-						p[i + 1].x.to(Units::mm) };
-		float vy[4] = { k * sin(p[i].phi + aoff),
-						k * sin(p[i + 1].phi + aoff),
-						p[i].y.to(Units::mm),
-						p[i + 1].y.to(Units::mm) };
-
-        /* invertierte hermitesche kubische Matrix (siehe Wikipedia)
-         *
-         * errechnet Splineparameter anhand gegebener Randwerte:
-         * - Position p
-         * - Tangente bzw. Winkel m
-         *
-         *  v = [m0 m1 p0 p1]' siehe oben
-         */
-        int m[4][4]=   {{ 1, 1, 2,-2},
-                        {-2,-1,-3, 3},
-                        { 1, 0, 0, 0},
-                        { 0, 0, 1, 0}};
-
-        /*
-         * Matrixmultiplikation c = M * v
-         */
-		for (i = 0; i < 4; i++) {
-            c.x.c[i] = 0.0f;
-            c.y.c[i] = 0.0f;
-
-            for (int j = 0; j < 4; j++) {
-				c.x.c[i] += static_cast<float>(m[i][j]) * vx[j];
-				c.y.c[i] += static_cast<float>(m[i][j]) * vy[j];
-            }
-        }
-
-        direct_dist = d12 * Units::mm;
-
-        bool ok = calculate_parameters();
-        return ok;
-    }
-
-	bool calc_spline_hermite(Pose *poses, unsigned pose_index)
-    {
-        //static_assert(order == 5, "wrong spline order for this call");
-
-        Pose *p = poses;
-        unsigned i = pose_index;
-
-        // see getPoseStep()
-        Angle aoff = 0 * Units::deg;
-        if (drive_direction == -1) {
-            aoff = 180 * Units::deg;
-        }
-
-        // spline form depends from distance between p[i] and p[i+1]
-        float d12 = distance(p[i], p[i+1]).to(Units::mm);
-        float k = spline_form_factor * d12;
-
-        float vx[4] = { p[i].x.to(Units::mm),
-						k * cos(p[i].phi + aoff),
-                        p[i + 1].x.to(Units::mm),
-						k * cos(p[i + 1].phi + aoff) };
-        float vy[4] = { p[i].y.to(Units::mm),
-						k * sin(p[i].phi + aoff),
-                        p[i + 1].y.to(Units::mm),
-						k * sin(p[i + 1].phi + aoff) };
-
-        int m[3][4] =  {{ -6,-3,  6,-3},
-                        { 15, 8,-15, 7},
-                        {-10,-6, 10,-4}};
-
-        c.x.c[3] = 0.0f;
-        c.y.c[3] = 0.0f;
-        c.x.c[4] = vx[1];
-        c.y.c[4] = vy[1];
-        c.x.c[5] = vx[0];
-        c.y.c[5] = vy[0];
-
-		for (i = 0; i < 3; i++) {
-            c.x.c[i] = 0.0f;
-            c.y.c[i] = 0.0f;
-
-            for (int j = 0; j < 4; j++) {
-				c.x.c[i] += static_cast<float>(m[i][j]) * vx[j];
-				c.y.c[i] += static_cast<float>(m[i][j]) * vy[j];
-
-            }
-        }
-
-        direct_dist = d12 * Units::mm;
-
-        bool ok = calculate_parameters();
-        return ok;
-    }
-
-	bool calculate_parameters(void)
-    {
-        static_assert(order == 3 || order == 5, "wrong spline order for this call");
-
-        float t, dx, dy, ddx, ddy;
-
-        float delta_t = spline_iteration_distance / direct_dist;
-        Point p_old = {c.x.getNode(order) * Units::mm, c.y.getNode(order) * Units::mm};
-
-        // max Krümmung
-        kappa_max = 0.f;
-
-        // Bogenlänge
-        length = 0 * Units::mm;
-
-        // Abtastung des Polynoms
-        for (t = delta_t; t <= 1.0f + delta_t; t = t + delta_t) {
-            // Wert an Stelle t
-            Point p = {c.x.val(t) * Units::mm, c.y.val(t) * Units::mm};
-
-            // Gesamtlänge += Abschnittslänge
-            length += distance(p_old, p);
-
-            // merken der Werte für Abstandsberechnung im nächsten Iterationschritt
-            p_old = p;
-
-            // Wert der 1. Ableitung an Stelle t
-			dx = c.x.template val<1>(t);
-			dy = c.y.template val<1>(t);
-
-            // Wert der 2. Ableitung an Stelle t
-			ddx = c.x.template val<2>(t);
-			ddy = c.y.template val<2>(t);
-
-            // max Krümmung bestimmen
-            kappa_max = fmaxf(kappa_max, fabsf((dx * ddy - ddx * dy)) / cbcf(hypotf(dx, dy)));
-        }
-
-        // Begrenzung...
-
-        //...der Bogenlänge: min. spline_iteration_distance
-        length = max(length, spline_iteration_distance);
-
-        //...der Krümmung: min. 1e-10f
-        kappa_max = fmaxf(kappa_max, 1e-10f);
-
-        return true;
-    }
-
-    static constexpr std::size_t Order = order;
+	void calc_spline(Pose *poses, unsigned pose_index);
+	bool calculate_parameters();
 
     Polynomial2D<order> c;
-    Length direct_dist;
-    Length length;
-    float kappa_max;
-    int drive_direction;
 };
 
 using Spline5 = SplineOrder<5>;
