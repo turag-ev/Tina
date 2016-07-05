d310b33d162df2d45982c94bf8b66a3e31483dac
Martin Oemus <martin@oemus.net>
2016-07-05 16:14:36 +0200

    [tina++] CheapErrorObserver und CheapErrorLogger hinzugefügt

--- tina/tina++/debug/errorobserver.cpp
+++ tina/tina++/debug/errorobserver.cpp
@@ -2,6 +2,7 @@
 
 #include <tina/debug/internal-debug.h>
 
+
 namespace TURAG {
 namespace Debug {
 
@@ -17,11 +18,11 @@ bool ErrorObserver::doErrorOutput(bool success)
 		++failure_counter_;
 
 		// Fehler
-		SystemTime now = SystemTime::now();
-		if (now - last_error_message_ > interval_ || failure_counter_ <= min_errors_) {
+		unsigned now_ms = SystemTime::now().toMsec();
+		if (now_ms - last_error_message_ms_ > interval_ms_ || failure_counter_ <= min_errors_) {
 
 			// Fehlermeldung ausgeben
-			last_error_message_ = now;
+			last_error_message_ms_ = now_ms;
 			failuresSinceLastOutput_ = failure_counter_ - failuresOnLastOutput_;
 			successesSinceLastOutput_ = success_counter_ - successesOnLastOutput_;
 			failuresOnLastOutput_ = failure_counter_;
@@ -41,7 +42,26 @@ void ErrorObserver::reset()
 	successesOnLastOutput_ = 0;
 	failuresSinceLastOutput_ = 0;
 	successesSinceLastOutput_ = 0;
-	last_error_message_ = SystemTime(0);
+	last_error_message_ms_ = 0;
+}
+
+
+
+bool CheapErrorObserver::doErrorOutput(bool success)
+{
+	if (!success)
+	{
+		unsigned now_ms = static_cast<uint32_t>(SystemTime::now().toMsec());
+		++failures_;
+
+		if ((last_error_message_ms_ == 0) || (now_ms - last_error_message_ms_ > interval_ms_))
+		{
+			last_error_message_ms_ = now_ms;
+			return true;
+		}
+	}
+
+	return false;
 }
 
 } // namespace Debug
--- tina/tina++/debug/errorobserver.h
+++ tina/tina++/debug/errorobserver.h
@@ -7,11 +7,11 @@ namespace TURAG {
 namespace Debug {
 
 /// \ingroup Debug
-/// \brief Fehlerüberwacher
+/// \brief Universeller Fehlerüberwacher.
 ///
 /// Die Klasse dient dazu häufige Fehlermeldungen nicht sofort
 /// auszugeben, sondern nur in bestimmten Abständen, damit
-/// die Debugausgabe nicht "zugespämt" wird. Kann aber auch für andere
+/// die Debugausgabe nicht zugespamt wird. Kann aber auch für andere
 /// ähnliche Sachen benutzt werden.
 ///
 /// In folgenden Beispiel liegen mindestens 25 Sekunden zwischen jeder Fehlermeldung:
@@ -36,12 +36,12 @@ public:
 	/// \param min_errors Fehler die mindestens ausgegeben werden, bevor die Ausgabe reduziert wird.
 	constexpr
 	ErrorObserver(SystemTime min_interval, unsigned min_errors = 5) :
-		interval_(min_interval), min_errors_(min_errors)
+		interval_ms_(min_interval.toMsec()), min_errors_(min_errors)
 	{ }
 
 	// verändern
 
-	/// \brief Abfrage, ob Ausgabe erfolgen soll, bei neuer Operationsausführung
+	/// \brief Abfrage ob Ausgabe erfolgen soll.
 	///
 	/// \param success Operation war erfolgreich
 	/// \returns \a true, wenn Fehlerausgabe fällig ist
@@ -55,7 +55,7 @@ public:
 	// Parameter auslesen
 
 	/// \brief minimales Intervall in dem Fehlermeldungen ausgegeben werden dürfen
-	const SystemTime& getMinInterval() const { return interval_; }
+	const SystemTime getMinInterval() const { return SystemTime::fromMsec(interval_ms_); }
 
 	// Werte auslesen
 
@@ -100,7 +100,7 @@ public:
 	///
 	/// Wenn noch kein Fehler aufgetretten ist (getErrorCount() == 0), dann
 	/// wird `SystemTime(0)` zurück gegeben.
-	constexpr const SystemTime& getLastErrorMessageTime() const { return last_error_message_;}
+	constexpr const SystemTime getLastErrorMessageTime() const { return SystemTime::fromMsec(last_error_message_ms_); }
 
 private:
 	/// Fehlerzähler
@@ -114,13 +114,109 @@ private:
 	unsigned successesSinceLastOutput_ = 0;
 
 	/// letzter Fehler
-	SystemTime last_error_message_ = SystemTime(0);
+	unsigned last_error_message_ms_ = 0;
 
 	/// minimales Intervall in dem Fehlermeldungen ausgegeben werden dürfen
-	SystemTime interval_;
+	unsigned interval_ms_;
 
 	unsigned min_errors_;
 };
+
+
+/// \ingroup Debug
+/// \brief Einfacher Fehlerüberwacher mit geringem Speicherbedarf.
+///
+/// Die Klasse dient dazu häufige Fehlermeldungen nicht sofort
+/// auszugeben, sondern nur in bestimmten Abständen, damit
+/// die Debugausgabe nicht zugespamt wird. Kann aber auch für andere
+/// ähnliche Sachen benutzt werden.
+///
+/// Diese Klasse ist gegenüber ErrorObserver deutlich leichtgewichtiger
+/// im Speicherverbrauch (üblicherweise 12 Byte). Dafür stellt sie
+/// als einzige Information die Gesamtfehleranzahl zur Verfügung.
+/// Wird eine Information über die seit der letzten Ausgabe aufgetretenen
+/// Fehleranzahl benötigt, so kann der Fehlerzähler mit resetErrorCount()
+/// manuell zurückgesetzt werden.
+///
+/// In folgenden Beispiel liegen mindestens 25 Sekunden zwischen jeder Fehlermeldung:
+/// \code
+/// static CheapErrorObserver error_observer(SystemTime::fromSec(25)); // Definition
+/// void getGpsPos() {
+///   Point point;
+///   if (error_observer.doErrorOutput(GPS::getPosition(&point))) {
+///     // Zeit für neue Ausgabe ist gekommen, da letzter Fehler ausrechend entfernt oder
+///     // noch nie ein Fehler aufgetretten
+///     turag_errorf("Konnte schon zum %u. mal keine GPS-Position lesen.", error_observer.getErrorCount());
+///   }
+/// }
+///
+/// \endcode
+class CheapErrorObserver
+{
+public:
+	// ctors
+
+	/// Fehlerüberwacher erstellen
+	/// \param min_interval minimales Intervall in dem Fehlermeldungen
+	/// ausgegeben werden dürfen.
+	CheapErrorObserver(SystemTime min_interval) :
+		interval_ms_(min_interval.toMsec())
+	{ }
+
+
+	// Parameter auslesen
+
+	/**
+	 * @brief Anzahl der aufgetretenen Fehler.
+	 *
+	 * Kann mit resetErrorCount() zurückgesetzt werden.
+	 */
+	unsigned getErrorCount(void)
+	{
+		return failures_;
+	}
+
+
+	// verändern
+
+	/// \brief Abfrage ob Ausgabe erfolgen soll.
+	///
+	/// \param success Operation war erfolgreich
+	/// \returns \a true, wenn Fehlerausgabe fällig ist
+	bool doErrorOutput(bool success);
+
+	/**
+	 * @brief Fehlerüberwacher zurücksetzen
+	 *
+	 * Fehlerzähler sowie letzten Fehlerzeitpunkt zurücksetzen.
+	 */
+	void resetAll()
+	{
+		last_error_message_ms_ = 0;
+		failures_ = 0;
+	}
+
+	/**
+	 * @brief Fehlerzähler zurücksetzen.
+	 */
+	void resetErrorCount(void)
+	{
+		failures_ = 0;
+	}
+
+
+private:
+	/// minimales Intervall in dem Fehlermeldungen ausgegeben werden dürfen
+	unsigned interval_ms_;
+
+	/// letzter Fehler
+	unsigned last_error_message_ms_ = 0;
+
+	/// Fehlerzähler
+	unsigned failures_ = 0;
+};
+
+
 
 } // namespace Debug
 } // namespace TURAG
