9e5d1d2ece985034642c4ab9cd18a3a2876c0b1e
Hermann von Kleist <hermann.von_kleist@turag.de>
2018-05-29 18:55:00 +0200

    [TinA] Stream-Operator entfernt

--- tina/tina++/geometry/riemanangle.h
+++ tina/tina++/geometry/riemanangle.h
@@ -1,10 +1,6 @@
 #ifndef RIEMANANGLE_H
 #define RIEMANANGLE_H
 
-#ifdef SIMULATION
-#include <iostream>
-#endif
-
 #include "../units.h"
 
 namespace TURAG {
@@ -21,14 +17,6 @@ struct RiemanAngle {
     bool operator <=(const RiemanAngle other) const;
     bool operator >=(const RiemanAngle other) const;
 
-#ifdef SIMULATION
-    template<class C, class T>
-    friend
-    std::basic_ostream<C, T>& operator <<(std::basic_ostream<C, T>& os, const RiemanAngle ra)
-    {
-        return os << "[" << ra.angle_.to(Units::rad) << " rad; " << ra.rieman_ << "]";
-    }
-#endif
 
     const Units::Angle& angle = angle_;
     const int& rieman = rieman_;
