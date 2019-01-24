a9e96f13cd8a328e56dfff82257d083b67918414
Hermann von Kleist <hermann.von_kleist@turag.de>
2019-01-24 21:24:32 +0100

    [TinA, SMTB] Stellantriebe-Slave-Implementierung tut

--- tina/tina++/feldbus/slave/feldbus_slave_stellantriebe.h
+++ tina/tina++/feldbus/slave/feldbus_slave_stellantriebe.h
@@ -11,12 +11,20 @@
  * 
  */
 
+#include <array>
 #include "feldbus_slave_base.h"
 #include "tina/feldbus/protocol/turag_feldbus_fuer_stellantriebe.h"
 
+#if (TURAG_FELDBUS_DEVICE_PROTOCOL==TURAG_FELDBUS_DEVICE_PROTOCOL_STELLANTRIEBE) || defined(__DOXYGEN__)
+
+#ifndef TURAG_FELDBUS_STELLANTRIEBE_STRUCTURED_OUTPUT_BUFFER_SIZE
+# warning TURAG_FELDBUS_STELLANTRIEBE_STRUCTURED_OUTPUT_BUFFER_SIZE must be defined, using default 0
+# define TURAG_FELDBUS_STELLANTRIEBE_STRUCTURED_OUTPUT_BUFFER_SIZE 0
+#endif
+
 namespace TURAG {
 namespace Feldbus {
-    
+
 /**
  * @brief Feldbus-Slave-Klassen und Debug-Funktionen.
  * @ingroup feldbus-slave-stellantriebe
@@ -36,64 +44,113 @@ public:
     /**
      * @brief Gibt an, ob ein Wert über das Stellantriebe-Protokoll geschrieben werden kann.
      */
-    enum class Access : bool {
-        RO = false,
-        RW = true,
+    enum class Access : uint8_t
+    {
+        READ = TURAG_FELDBUS_STELLANTRIEBE_COMMAND_ACCESS_NO_WRITE_ACCESS,
+        WRITE = TURAG_FELDBUS_STELLANTRIEBE_COMMAND_ACCESS_WRITE_ACCESS,
     };
 
     /**
-     * @brief Stellantriebe-Befehl
-     * 
-     * Diese Datenstruktur soll nicht direkt, sondern mit der funktion mk_cmd instanziiert werden.
+     * @brief Gibt Länge bzw. Typ eines Befehls an
      */
-    struct Command {
-        void* value;
-        const char *name;
-        float factor;
-        uint8_t id;
-        uint8_t length;
-        Access access;
+    enum class CommandType : uint8_t
+    {
+        NONE = TURAG_FELDBUS_STELLANTRIEBE_COMMAND_LENGTH_NONE,
+        CHAR = TURAG_FELDBUS_STELLANTRIEBE_COMMAND_LENGTH_CHAR,
+        SHORT = TURAG_FELDBUS_STELLANTRIEBE_COMMAND_LENGTH_SHORT,
+        LONG = TURAG_FELDBUS_STELLANTRIEBE_COMMAND_LENGTH_LONG,
+        NONE_TEXT = TURAG_FELDBUS_STELLANTRIEBE_COMMAND_LENGTH_NONE_TEXT,
     };
+
+    struct CommandInfo
+    {
+        Access access;
+        CommandType type;
+        float factor;
+    } _packed;
+
+    static constexpr
+    size_t get_command_length(CommandType t)
+    {
+        // constexpr does not allow switch :(
+        return t == CommandType::CHAR
+                ? sizeof(int8_t)
+                : t == CommandType::SHORT
+                  ? sizeof(int16_t)
+                  : t == CommandType::LONG
+                    ? sizeof(int32_t)
+                    : 0;
+    }
+
+    struct Command
+    {
+        constexpr
+        Command() :
+            info{Access::READ, CommandType::NONE,
+                 TURAG_FELDBUS_STELLANTRIEBE_COMMAND_FACTOR_CONTROL_VALUE},
+            value(nullptr), name(nullptr)
+        { }
+
+        constexpr
+        Command(const char* name) :
+            info{Access::READ, CommandType::NONE_TEXT,
+                 TURAG_FELDBUS_STELLANTRIEBE_COMMAND_FACTOR_CONTROL_VALUE},
+            value(nullptr), name(name)
+        { }
+
+        constexpr
+        Command(const char* name, int8_t* value, float factor,
+                Access access = Access::READ) :
+            info{access, CommandType::CHAR, factor}, value(value), name(name)
+        { }
+
+        constexpr
+        Command(const char* name, int16_t* value, float factor,
+                Access access = Access::READ) :
+            info{access, CommandType::SHORT, factor}, value(value), name(name)
+        { }
+
+        constexpr
+        Command(const char* name, int32_t* value, float factor,
+                Access access = Access::READ) :
+            info{access, CommandType::LONG, factor}, value(value), name(name)
+        { }
+
+        template<typename T>
+        constexpr
+        Command(const char* name, T* value, Access access = Access::READ) :
+            Command(name, value,
+                    TURAG_FELDBUS_STELLANTRIEBE_COMMAND_FACTOR_CONTROL_VALUE,
+                    access)
+        { }
+
+        CommandInfo info;
+        void *value;
+        const char *name;
+    };
+
+
+    using CommandUpdateHandler = void (*)(const Command &command);
 
     /**
      * @brief Initializiert die Stellantriebe-Implementierung.
      * @param[in] commands Array mit Stellantriebe-Befehlen
-     * @param[in] packetProcessor Funktion, die Paket verarbeitet, falls das nicht konform zum Stellantriebe-Protokoll ist
+     * @param[in] packetProcessor Funktion, die Paket verarbeitet,
+     * falls das nicht konform zum Stellantriebe-Protokoll ist
      * @param[in] broadcastProcessor Funktion, die Broadcast-Pakete verarbeitet
      */
-    template<size_t N>
-    static
-    void init(
-        const Command (&commands)[N],
-        Base::PacketProcessor packetProcessor = nullptr,
-        Base::BroadcastProcessor broadcastProcessor = nullptr)
+    template <size_t N>
+    static void init(
+            const Command (&commands)[N],
+            CommandUpdateHandler commandUpdateHandler = nullptr,
+            Base::PacketProcessor packetProcessor = nullptr,
+            Base::BroadcastProcessor broadcastProcessor = nullptr)
     {
         Base::init(&Stellantriebe::process_feldbus_packet, broadcastProcessor);
         backup_packet_processor_ = packetProcessor;
+        command_update_handler_ = commandUpdateHandler;
         command_set_ = commands;
         command_set_size_ = N;
-    }
-
-    /**
-     * @brief Erzeugt einen Stellantriebe-Befehl.
-     * @param[in] id Key des Stellantriebe-Befehls
-     * @param[in] value Pointer auf den Wert
-     * @param[in] name String, der Befehl verbal beschreibt
-     * @param[in] factor Umrechnungsfaktor zu Einheit
-     * @param[in] access Zugriffsart des Stellantriebe-Befehls
-     * @return Stellantriebe-Befehl
-     */
-    template <typename T>
-    static constexpr const
-    Command mk_cmd(
-        uint8_t id,
-        T *value,
-        const char *name,
-        float factor = 1.0f, 
-        Access access = Access::RO)
-    {
-        static_assert(sizeof(T) <= 4, "Stellantriebe command must not be larger than 4 bytes!");
-        return { value, name, factor, id, sizeof(T), access};
     }
 
     /**
@@ -103,13 +160,24 @@ public:
      * @param[out] Ausgabepuffer der Antwort
      * @return Länge der Antwortnachricht
      */
-    static FeldbusSize_t process_feldbus_packet(const uint8_t* message, FeldbusSize_t message_length, uint8_t* response);
+    static FeldbusSize_t process_feldbus_packet(
+            const uint8_t* message,
+            FeldbusSize_t message_length,
+            uint8_t* response);
 private:
     Stellantriebe();
 
     static Base::PacketProcessor backup_packet_processor_;
+    static CommandUpdateHandler command_update_handler_;
+
     static const Command* command_set_;
+    static std::array<
+        const Command*,
+        TURAG_FELDBUS_STELLANTRIEBE_STRUCTURED_OUTPUT_BUFFER_SIZE
+    > structured_output_table_;
+
     static size_t command_set_size_;
+    static size_t structured_output_table_size_;
 };
 
 
@@ -117,4 +185,5 @@ private:
 } // namespace Feldbus
 } // namespace TURAG
 
+#endif /* (TURAG_FELDBUS_DEVICE_PROTOCOL==TURAG_FELDBUS_DEVICE_PROTOCOL_STELLANTRIEBE) */
 #endif /* SLAVE_FELDBUS_SLAVE_STELLANTRIEBE_H_ */
