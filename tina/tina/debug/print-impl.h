85a72388efe7057ce7d89ddc5f854e3a48c7ac92
Hermann von Kleist <hermann.von_kleist@turag.de>
2019-02-23 00:52:40 +0100

    [TinA] Mehr Fixes für TURAG_DEBUG_LEVEL=0

--- tina/tina/debug/print-impl.h
+++ tina/tina/debug/print-impl.h
@@ -7,6 +7,9 @@ TURAG_C_API_BEGIN
 
 #ifndef __DOXYGEN__
 
+// turag_debug_* Implementation wählen
+#if TURAG_DEBUG_LEVEL > 0
+
 // gibt im Unterschied zu turag_debug_puts in Regelmäßigen Abständen die Spielzeit aus
 void turag_log_puts(const char* s);
 
@@ -15,8 +18,6 @@ void turag_log_printf(const char* fmt, ...) __attribute__ ((format(printf, 1, 2)
 
 void turag_debug_printf(const char* fmt, ...) __attribute__ ((format(printf, 1, 2)));
 
-// turag_debug_* Implementation wählen
-#if TURAG_DEBUG_LEVEL > 0
 # if TURAG_PRINT_GAMETIME_AUTOMATIC
 #  define turag_debug_puts_impl turag_log_puts
 #  define turag_debug_printf_impl turag_log_printf
@@ -24,12 +25,18 @@ void turag_debug_printf(const char* fmt, ...) __attribute__ ((format(printf, 1, 
 #  define turag_debug_puts_impl turag_debug_puts
 #  define turag_debug_printf_impl turag_debug_printf
 # endif // TURAG_PRINT_GAMETIME_AUTOMATIC
-#else
+
+#else  // TURAG_DEBUG_LEVEL > 0
+
 # define turag_debug_puts_impl(...) TURAG_MACRO_NOOP
 # define turag_debug_printf_impl(...) TURAG_MACRO_NOOP
-#endif
+# define turag_log_puts(...) TURAG_MACRO_NOOP
+# define turag_log_printf(...) TURAG_MACRO_NOOP
+# define turag_debug_printf(...) TURAG_MACRO_NOOP
 
-#endif
+#endif  //  TURAG_DEBUG_LEVEL > 0
+
+#endif // __DOXYGEN __
 
 TURAG_C_API_END
 
