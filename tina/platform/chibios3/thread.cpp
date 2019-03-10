a518e46b3f94dc1b45fd850b2838cc021aec2eba
Lukas Müller <Lukas.Mueller4@mailbox.tu-dresden.de>
2019-03-10 22:39:55 +0100

    [tina++]getMainStackUsage und getProcessStackUsage

--- tina/platform/chibios3/thread.cpp
+++ tina/platform/chibios3/thread.cpp
@@ -16,35 +16,44 @@ void _turag_thread_entry(void* data) {
 
 #ifdef CH_DBG_FILL_THREADS
 
-std::size_t get_stack_usage(thread_t* t, std::size_t stack_size) {
-    const char* end = reinterpret_cast<const char*>(t->wabase);
+std::size_t get_stack_usage(const char* working_area_base, std::size_t stack_size) {
+    const char* end = working_area_base;
     const char* begin = end + stack_size;
     for (const char* ptr = end; ptr != begin; ptr++) {
         if (*ptr != 0x55) { // ChibiOS fills stack with 0x55 on initialization
             return (begin - ptr) * sizeof(char);
         }
     }
-    return 0;
+    return stack_size; //stack full
 }
 
 std::size_t ThreadImpl::getStackUsage() const {
-    return get_stack_usage(thread_, stack_size_);
+    return get_stack_usage(reinterpret_cast<const char*>(thread_->wabase), stack_size_);
 }
 
 extern "C"
 size_t turag_thread_get_stack_usage(const TuragThread* thread) {
-  return get_stack_usage(thread->thread, thread->stack_size);
+  return get_stack_usage(reinterpret_cast<const char*>(thread->thread->wabase), thread->stack_size);
 }
+
+
+extern "C" const char __main_stack_base__, __main_stack_end__;
+std::size_t getMainStackUsage() { 
+    return get_stack_usage(&__main_stack_base__, (&__main_stack_end__) - (&__main_stack_base__));
+}
+
+extern "C" const char __process_stack_base__, __process_stack_end__;
+std::size_t getProcessStackUsage() {
+    return get_stack_usage(&__process_stack_base__, (&__process_stack_end__) - (&__process_stack_base__));
+}
+
 #else // CH_DBG_FILL_THREADS
 
-std::size_t ThreadImpl::getStackUsage() const {
-    return 0;
-}
+std::size_t ThreadImpl::getStackUsage() const { return 0; }
+extern "C" size_t turag_thread_get_stack_usage(const TuragThread* thread) { return 0; }
 
-extern "C"
-size_t turag_thread_get_stack_usage(const TuragThread* thread) {
-  return 0;
-}
+std::size_t getMainStackUsage() { return 0; }
+std::size_t getProcessStackUsage() { return 0; }
 
 #endif // CH_DBG_FILL_THREADS
 
