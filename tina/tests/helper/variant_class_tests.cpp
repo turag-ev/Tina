6147c780693be7ff85d96e06bf2342685296d8a5
Hermann von Kleist <hermann.von_kleist@turag.de>
2020-01-25 01:38:11 +0100

    [TinA] VariantClass gefixt.
    Probleme traten auf bei
    - Nichttrivialen Kopier/Move-Konstruktoren
    - Mehrfachvererbung

--- tina/tests/helper/variant_class_tests.cpp
+++ tina/tests/helper/variant_class_tests.cpp
@@ -139,13 +139,18 @@ BOOST_AUTO_TEST_CASE(multi) {
 	Type variant;
 	BOOST_CHECK_EQUAL(variant.empty(), true);
 	BOOST_CHECK_EQUAL(variant.operator ->(), null);
+	BOOST_CHECK_GE(sizeof(Type), sizeof(A));
+	BOOST_CHECK_GE(sizeof(Type), sizeof(B));
+	BOOST_CHECK_EQUAL(variant.get(), null);
 
 	variant.emplace<A>();
 	BOOST_CHECK_EQUAL(variant.empty(), false);
 	BOOST_CHECK_EQUAL(variant->getValue(), 0xAAAA);
+	BOOST_CHECK_NE(variant.get(), null);
 
 	variant.emplace<B>();
 	BOOST_CHECK_EQUAL(variant.empty(), false);
+	BOOST_CHECK_EQUAL(variant->getValue(), 0xBBBB);
 
 	int value = variant->getValue();
 	BOOST_CHECK_EQUAL(value, 0xBBBB);
@@ -153,6 +158,162 @@ BOOST_AUTO_TEST_CASE(multi) {
 	variant.erase();
 	BOOST_CHECK_EQUAL(variant.empty(), true);
 	BOOST_CHECK_EQUAL(variant.operator ->(), null);
+
+	variant.emplace<A>();
+	BOOST_CHECK_EQUAL(variant.empty(), false);
+	BOOST_CHECK_EQUAL(variant->getValue(), 0xAAAA);
+
+	Type variant2(variant);
+	BOOST_CHECK_NE(variant.get(), variant2.get());
+}
+
+BOOST_AUTO_TEST_CASE(CopyMoveCtorSingleInherit) {
+	class Base {
+	public:
+		virtual ~Base() { }
+
+		virtual int getValue() const { return 0xBA5E; }
+	};
+
+	class A : public Base {
+	public:
+        A() {
+            defaults++;
+        }
+        A(const A& a) : defaults(a.defaults), copies(a.copies), moves(a.moves) {
+            copies++;
+        }
+        A(A&& a) : defaults(a.defaults), copies(a.copies), moves(a.moves) {
+            moves++;
+        }
+		int getValue() const override { return 0xAAAA; }
+        unsigned defaults = 0;
+        unsigned copies = 0;
+        unsigned moves = 0;
+	};
+
+	class B : public Base {
+	public:
+		int getValue() const override { return 0xBBBB; }
+	};
+
+	using Type = VariantClass<Base, A, B>;
+    
+    Type variant1 = B(); // construct by passing derived class (note: this is default construct and copy construct!)
+	BOOST_CHECK_EQUAL(variant1->getValue(), 0xBBBB);
+    variant1 = A(); // assign by passing derived class
+	BOOST_CHECK_EQUAL(variant1->getValue(), 0xAAAA);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant1).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant1).copies, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant1).moves, 0);
+    
+    Type variant2 = variant1; // copy construct
+	BOOST_CHECK_EQUAL(variant2->getValue(), 0xAAAA);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant2).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant2).copies, 2);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant2).moves, 0);
+    
+    Type variant3; // create empty
+    BOOST_CHECK_EQUAL(variant3.get(), null);
+    variant3 = variant1; // copy assign
+	BOOST_CHECK_EQUAL(variant3->getValue(), 0xAAAA);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant3).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant3).copies, 2);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant3).moves, 0);
+    
+    Type variant4 = std::move(variant1); // move construct
+	BOOST_CHECK_EQUAL(variant4->getValue(), 0xAAAA);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant4).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant4).copies, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant4).moves, 1);
+    
+    Type variant5; // create empty
+    BOOST_CHECK_EQUAL(variant5.get(), null);
+    variant5 = std::move(variant1); // move assign
+	BOOST_CHECK_EQUAL(variant5->getValue(), 0xAAAA);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant5).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant5).copies, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant5).moves, 1);
+}
+
+BOOST_AUTO_TEST_CASE(CopyMoveCtorMultiInherit) {
+	class Base {
+	public:
+		virtual ~Base() { }
+
+		virtual int getValue() const { return 0xBA5E; }
+	};
+
+	class Base2 {
+	public:
+		virtual ~Base2() { }
+
+		virtual unsigned get() const { return 0xBA5E2; }
+	};
+
+	class A : public Base, public Base2 {
+	public:
+        A() {
+            defaults++;
+        }
+        A(const A& a) : defaults(a.defaults), copies(a.copies), moves(a.moves) {
+            copies++;
+        }
+        A(A&& a) : defaults(a.defaults), copies(a.copies), moves(a.moves) {
+            moves++;
+        }
+		int getValue() const override { return 0xAAAA; }
+        unsigned defaults = 0;
+        unsigned copies = 0;
+        unsigned moves = 0;
+	};
+
+	class B : public Base2, public Base {
+	public:
+		int getValue() const override { return 0xBBBB; }
+	};
+
+	using Type = VariantClass<Base, A, B>;
+    
+    Type variant_a = A();
+    Type variant_b = B();
+	BOOST_CHECK_EQUAL(variant_a->getValue(), 0xAAAA);
+    BOOST_CHECK_EQUAL(static_cast<A&>(*variant_a).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant_a).copies, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant_a).moves, 0);
+	BOOST_CHECK_EQUAL(variant_b->getValue(), 0xBBBB);
+    
+    Type variant1 = variant_a; // copy construct a
+	BOOST_CHECK_EQUAL(variant1->getValue(), 0xAAAA);
+    BOOST_CHECK_EQUAL(static_cast<A&>(*variant1).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant1).copies, 2);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant1).moves, 0);
+    variant1 = variant_b; // copy assign b
+	BOOST_CHECK_EQUAL(variant1->getValue(), 0xBBBB);
+    
+    Type variant2 = variant_b; // copy construct b
+	BOOST_CHECK_EQUAL(variant2->getValue(), 0xBBBB);
+    variant2 = variant_a; // copy assign a
+	BOOST_CHECK_EQUAL(variant2->getValue(), 0xAAAA);
+    BOOST_CHECK_EQUAL(static_cast<A&>(*variant2).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant2).copies, 2);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant2).moves, 0);
+    
+    Type variant3 = std::move(variant_a); // move construct a
+	BOOST_CHECK_EQUAL(variant3->getValue(), 0xAAAA);
+    BOOST_CHECK_EQUAL(static_cast<A&>(*variant3).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant3).copies, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant3).moves, 1);
+    variant3 = std::move(variant_b); // move assign b
+	BOOST_CHECK_EQUAL(variant3->getValue(), 0xBBBB);
+    
+    Type variant4 = std::move(variant1); // move construct b
+	BOOST_CHECK_EQUAL(variant4->getValue(), 0xBBBB);
+    variant4 = std::move(variant2); // move assign a
+	BOOST_CHECK_EQUAL(variant4->getValue(), 0xAAAA);
+    BOOST_CHECK_EQUAL(static_cast<A&>(*variant4).defaults, 1);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant4).copies, 2);
+	BOOST_CHECK_EQUAL(static_cast<A&>(*variant4).moves, 1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tina/tina++/container/variant_class.h
+++ tina/tina++/container/variant_class.h
@@ -1,7 +1,7 @@
 #ifndef TINA_CONTAINER_VARIANT_CLASS_H
 #define TINA_CONTAINER_VARIANT_CLASS_H
 
-#include <cstdio>
+#include <cassert>
 #include <type_traits>
 
 #include <tina++/helper/traits_algorithms.h>
@@ -9,18 +9,108 @@
 
 namespace TURAG {
 
-template<class Abstract, class ...Variants>
+namespace detail {
+
+template<typename>
+void destruct_as_one_of(void*, const std::size_t) {
+    // This is reached if idx >= sizeof...(Variants)
+}
+
+template<typename, typename T0, typename... Ts>
+void destruct_as_one_of(void* obj, const std::size_t idx) {
+    if (idx) {
+        destruct_as_one_of<void, Ts...>(obj, idx - 1);
+    } else {
+        destruct(static_cast<T0*>(obj));
+    }
+}
+
+template<typename Base>
+Base* copy_construct_as_one_of(void*, const Base&, const std::size_t) {
+    // This is reached if idx >= sizeof...(Variants)
+    return nullptr;
+}
+
+template<typename Base, typename T0, typename... Ts>
+Base* copy_construct_as_one_of(void* target, const Base& source, const std::size_t idx) {
+    if (idx) {
+        return copy_construct_as_one_of<Base, Ts...>(target, source, idx - 1);
+    } else {
+        construct(reinterpret_cast<T0*>(target), static_cast<const T0&>(source));
+        return reinterpret_cast<T0*>(target);
+    }
+}
+
+template<typename Base>
+Base* move_construct_as_one_of(void*, Base&&, const std::size_t) {
+    // This is reached if idx >= sizeof...(Variants)
+    return nullptr;
+}
+
+template<typename Base, typename T0, typename... Ts>
+Base* move_construct_as_one_of(void* target, Base&& source, const std::size_t idx) {
+    if (idx) {
+        return move_construct_as_one_of<Base, Ts...>(target, std::forward<Base>(source), idx - 1);
+    } else {
+        construct(reinterpret_cast<T0*>(target), std::forward<T0>(static_cast<T0&&>(source)));
+        return reinterpret_cast<T0*>(target);
+    }
+}
+
+template<typename Base>
+Base* cast_as_one_of(void*, const std::size_t) {
+    // This is reached if idx >= sizeof...(Variants)
+    return nullptr;
+}
+
+template<typename Base, typename T0, typename... Ts>
+Base* cast_as_one_of(void* obj, const std::size_t idx) {
+    if (idx) {
+        return cast_as_one_of<Base, Ts...>(obj, idx - 1);
+    } else {
+        return reinterpret_cast<T0*>(obj);
+    }
+}
+
+template<typename Base>
+const Base* cast_as_one_of(const void*, const std::size_t) {
+    // This is reached if idx >= sizeof...(Variants)
+    return nullptr;
+}
+
+template<typename Base, typename T0, typename... Ts>
+const Base* cast_as_one_of(const void* obj, const std::size_t idx) {
+    if (idx) {
+        return cast_as_one_of<Base, Ts...>(obj, idx - 1);
+    } else {
+        return reinterpret_cast<const T0*>(obj);
+    }
+}
+
+template<typename T>
+constexpr std::size_t index_of_type(const std::size_t idx = 0) {
+    // This is reached if idx >= sizeof...(Variants)
+    return idx;
+}
+
+template<typename T, typename T0, typename... Ts>
+constexpr std::size_t index_of_type(const std::size_t idx = 0) {
+    return std::is_same<T, T0>::value ? idx : index_of_type<T, Ts...>(idx + 1);
+}
+
+} // namespace detail
+
+
+template<class Abstract, class... Variants>
 class VariantClass {
 	static_assert(all_true<std::is_base_of<Abstract, Variants>...>::value, "Nicht alle Klassen sind Implementierungen des ersten Templateparameters");
-    //static_assert(all_true<std::is_trivially_copyable<Variants>...>::value, "FIXME");
-
 public:
 
 	// cdtors
 
 	constexpr
 	VariantClass() :
-	data_(), active_(nullptr)
+	data_(), active_idx_(sizeof... (Variants)) // initialize with invalid value.
 	{ }
 
 	~VariantClass()
@@ -29,32 +119,39 @@ public:
 	}
 
     template<class T>
-    VariantClass(const T& rhs):
-		active_(reinterpret_cast<Abstract*>(&data_) )
+    VariantClass(const T& rhs) : VariantClass()
     {
-        static_assert(!all_false<std::is_same<T,Variants>...>::value, "Can't construct from non-variant class.");
-		construct(reinterpret_cast<T*>(&data_), rhs);
+        emplace<T>(rhs);
 	}
 
-	VariantClass(const VariantClass& rhs)
+	VariantClass(const VariantClass& rhs) : VariantClass()
     {
-		*this = rhs;
+        if (rhs) {
+		    detail::copy_construct_as_one_of<Abstract, Variants...>(&data_, *rhs, rhs.active_idx_);
+            active_idx_ = rhs.active_idx_;
+        }
 	}
 
-	const VariantClass& operator=(const VariantClass &rhs)
+	VariantClass(VariantClass&& rhs) : VariantClass()
+    {
+        if (rhs) {
+            detail::move_construct_as_one_of<Abstract, Variants...>(&data_, std::forward<Abstract>(*rhs), rhs.active_idx_);
+            active_idx_ = rhs.active_idx_;
+        }
+	}
+
+	VariantClass operator=(VariantClass rhs)
 	{
-		memcpy(&data_, &rhs.data_, max_value(sizeof(Variants)...) );
-        if(rhs.empty())
-            active_ = nullptr;
-        else
-			active_ = reinterpret_cast<Abstract*>(&data_);
+        // copy-and-swap idiom
+        std::swap(data_, rhs.data_);
+        std::swap(active_idx_, rhs.active_idx_);
 		return *this;
     }
 
     // empty
 	constexpr
 	bool empty() const {
-		return active_ == nullptr;
+		return active_idx_ >= sizeof... (Variants);
 	}
 
 	explicit constexpr
@@ -65,43 +162,41 @@ public:
 	// get pointer
 
 	Abstract* get() {
-		return active_;
+		return detail::cast_as_one_of<Abstract, Variants...>(&data_, active_idx_);
 	}
 
 	constexpr
 	const Abstract* get() const {
-		return active_;
+		return detail::cast_as_one_of<Abstract, Variants...>(&data_, active_idx_);
 	}
 
 	Abstract* operator -> () {
-		return active_;
+		return get();
 	}
 
 	constexpr
 	const Abstract* operator -> () const {
-		return active_;
+		return get();
 	}
 
 	// get reference
 
 	Abstract& operator * () {
-		return *active_;
+		return *get();
 	}
 
 	constexpr
 	const Abstract& operator * () const {
-		return *active_;
+		return *get();
 	}
 
 	// modify
 
 	void erase() {
-		if (empty()) return;
-
-		Abstract* old_ptr = active_;
-		active_ = nullptr;
-
-		destruct(old_ptr);
+		if (empty())
+            return;
+        detail::destruct_as_one_of<void, Variants...>(reinterpret_cast<Abstract*>(&data_), active_idx_);
+        active_idx_ = sizeof...(Variants);
 	}
 
 	template<typename T, typename ...Args>
@@ -111,8 +206,8 @@ public:
 				"Typ muss zu VariantClass-Definition als Parameter hinzugefügt werden.");
 		erase();
 		construct(reinterpret_cast<T*>(&data_), std::forward<Args>(args)...);
-		active_ = static_cast<Abstract*>(reinterpret_cast<T*>(&data_));
-        //assert(active_ != nullptr);
+        active_idx_ = detail::index_of_type<T, Variants...>();
+        assert(active_idx_ < sizeof... (Variants));
 	}
 
 private:
@@ -123,7 +218,7 @@ private:
 #else
 	UnionStorage<Variants...> data_;
 #endif
-    Abstract* active_;
+    std::size_t active_idx_;
 };
 
 } // namespace TURAG
