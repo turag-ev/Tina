53ca8965aa2d30ca36b3015c78a3fe65dd2477dc
Lukas Müller <Lukas.Mueller4@mailbox.tu-dresden.de>
2020-03-10 13:37:28 +0100

    [tina]eCos entfernt

--- tina/platform/ecos/public/tina/config_tina_platform.h
+++
@@ -1,9 +0,0 @@
-#ifndef TINA_CONFIG_TINA_PLATFORM_H
-#define TINA_CONFIG_TINA_PLATFORM_H
-
-/// ASCII-Zeichen für neue Zeile für Debugausgaben
-#define TURAG_DEBUG_NEWLINE "\r\n"
-
-#define __WORDSIZE 32
-
-#endif // TINA_CONFIG_TINA_PLATFORM_H
--- tina/platform/ecos/public/tina/testmenu.h
+++
@@ -1,7 +0,0 @@
-#ifndef TINA_ECOS_TESTMENU_H
-#define TINA_ECOS_TESTMENU_H
-
-# include <turag/console_menu.h>
-# include <turag/boardtestmenu.h>
-
-#endif // TINA_ECOS_TESTMENU_H
