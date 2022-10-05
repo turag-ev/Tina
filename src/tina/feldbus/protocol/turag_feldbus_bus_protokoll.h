8dfbe076ad28b48343afe987db9e425ce509cb76
Martin Oemus <martin@oemus.net>
2022-10-05 12:11:22 +0200

    feldbus: updated protocol header

--- src/tina/feldbus/protocol/turag_feldbus_bus_protokoll.h
+++ src/tina/feldbus/protocol/turag_feldbus_bus_protokoll.h
@@ -9,8 +9,8 @@
 
 
 
-#ifndef TINA_FELDBUS_PROTOCOL_TURAG_FELDBUS_BUS_PROTOKOLL_H
-#define TINA_FELDBUS_PROTOCOL_TURAG_FELDBUS_BUS_PROTOKOLL_H
+#ifndef TURAG_FELDBUS_BUS_PROTOKOLL_H_
+#define TURAG_FELDBUS_BUS_PROTOKOLL_H_
 
 /**
  * @name Special bus addresses
@@ -37,7 +37,9 @@
 /// @brief Xor-based checksum.
 #define TURAG_FELDBUS_CHECKSUM_XOR				0x00
 /// @brief CRC8-Checksum.
-#define TURAG_FELDBUS_CHECKSUM_CRC8_ICODE		0x01
+#define TURAG_FELDBUS_CHECKSUM_CRC8				0x01
+
+#define TURAG_FELDBUS_CHECKSUM_CRC8_16_MIXED   	0x02
 
 ///@}
 
@@ -45,7 +47,7 @@
  * @name Device Protocol Types and broadcast addresses
  * @{
  */
- 
+
 /// @brief Broadcast to all device types
 #define TURAG_FELDBUS_BROADCAST_TO_ALL_DEVICES					0x00
 
@@ -71,36 +73,87 @@
  * @name Reservierte Pakete
  * @{
  */
- 
+
 /// @brief Ausgeben des Gerätenamens
-#define TURAG_FELDBUS_SLAVE_COMMAND_DEVICE_NAME						0x00
+#define TURAG_FELDBUS_DEVICE_COMMAND_DEVICE_NAME					0x00
 
 /// @brief Ausgeben des Uptime-Counters
-#define TURAG_FELDBUS_SLAVE_COMMAND_UPTIME_COUNTER					0x01
+#define TURAG_FELDBUS_DEVICE_COMMAND_UPTIME_COUNTER					0x01
 
 /// @brief Ausgeben des Versionsinfo-Strings
-#define TURAG_FELDBUS_SLAVE_COMMAND_VERSIONINFO						0x02
+#define TURAG_FELDBUS_DEVICE_COMMAND_VERSIONINFO					0x02
 
 /// @brief Ausgabe der Anzahl korrekt empfangener Pakete
-#define TURAG_FELDBUS_SLAVE_COMMAND_PACKAGE_COUNT_CORRECT			0x03
+#define TURAG_FELDBUS_DEVICE_COMMAND_PACKAGE_COUNT_CORRECT			0x03
 
 /// @brief Ausgabe der Anzahl von Pufferüberläufen
-#define TURAG_FELDBUS_SLAVE_COMMAND_PACKAGE_COUNT_BUFFEROVERFLOW	0x04
+#define TURAG_FELDBUS_DEVICE_COMMAND_PACKAGE_COUNT_BUFFEROVERFLOW	0x04
 
 /// @brief Ausgabe der Anzahl verlorener Pakete
-#define TURAG_FELDBUS_SLAVE_COMMAND_PACKAGE_COUNT_LOST				0x05
+#define TURAG_FELDBUS_DEVICE_COMMAND_PACKAGE_COUNT_LOST				0x05
 
 /// @brief Ausgabe der Anzahl von Checksummenfehlern
-#define TURAG_FELDBUS_SLAVE_COMMAND_PACKAGE_COUNT_CHKSUM_MISMATCH	0x06
+#define TURAG_FELDBUS_DEVICE_COMMAND_PACKAGE_COUNT_CHKSUM_MISMATCH	0x06
 
 /// @brief Ausgabe aller vier Paketanzahlen in folgender Reihenfolge: korrekte Pakete, Pufferüberläufe, verlorene Pakete, Checksummenfehler
-#define TURAG_FELDBUS_SLAVE_COMMAND_PACKAGE_COUNT_ALL				0x07
+#define TURAG_FELDBUS_DEVICE_COMMAND_PACKAGE_COUNT_ALL				0x07
 
 /// @brief Alle 4 Paketanzahlen werden auf 0 zurückgesetzt
-#define TURAG_FELDBUS_SLAVE_COMMAND_RESET_PACKAGE_COUNT				0x08
+#define TURAG_FELDBUS_DEVICE_COMMAND_RESET_PACKAGE_COUNT			0x08
+
+/// @brief Returns UUID of the device
+#define TURAG_FELDBUS_DEVICE_COMMAND_GET_UUID						0x09
+
+/// @brief Return the extended device info packet. Not available when the device returns a legacy style device info packet.
+#define TURAG_FELDBUS_DEVICE_COMMAND_GET_EXTENDED_INFO				0x0A
+
+/// @brief Return the capacity of the static data storage and its page size. For write operations
+/// valid values of the address offset are limited to multiples of the page size.
+#define TURAG_FELDBUS_DEVICE_COMMAND_GET_STATIC_STORAGE_CAPACITY	0x0B
+
+/// @brief Read data from the static data storage from the specified address with the specified length.
+#define TURAG_FELDBUS_DEVICE_COMMAND_READ_FROM_STATIC_STORAGE		0x0C
+
+/// @brief Write data to the static data storage at the specified address. Returns 0 on success, an error code on error.
+#define TURAG_FELDBUS_DEVICE_COMMAND_WRITE_TO_STATIC_STORAGE		0x0D
+
+
+///@}
+/**
+ * @name Reserved broadcasts with broadcast ID 0x00
+ * @{
+ */
+
+/// @brief Ping any device to return UUID
+#define TURAG_FELDBUS_DEVICE_BROADCAST_UUID						0x00
+
+/// @brief get/set bus address of device with given UUID
+#define TURAG_FELDBUS_DEVICE_BROADCAST_UUID_ADDRESS				0x00
+
+/// @brief reset bus address of specified device
+#define TURAG_FELDBUS_DEVICE_BROADCAST_UUID_RESET_ADDRESS		0x01
+
+/// @brief enable bus neighbors
+#define TURAG_FELDBUS_DEVICE_BROADCAST_ENABLE_NEIGHBOURS		0x01
+
+/// @brief disable bus neighbors
+#define TURAG_FELDBUS_DEVICE_BROADCAST_DISABLE_NEIGHBOURS		0x02
+
+/// @brief disable bus neighbors
+#define TURAG_FELDBUS_DEVICE_BROADCAST_RESET_ADDRESSES			0x03
+
+/// @brief disable bus neighbors
+#define TURAG_FELDBUS_DEVICE_BROADCAST_REQUEST_BUS_ASSERTION	0x04
+
+/// @brief disable bus neighbors if no valid bus address
+#define TURAG_FELDBUS_DEVICE_BROADCAST_REQUEST_BUS_ASSERTION_IF_NO_ADRRESS	0x05
+
+/// @brief disable bus neighbors if no valid bus address
+#define TURAG_FELDBUS_DEVICE_BROADCAST_GO_TO_SLEEP				0x06
+
 
 
 ///@}
 
 
-#endif // TINA_FELDBUS_PROTOCOL_TURAG_FELDBUS_BUS_PROTOKOLL_H
+#endif
